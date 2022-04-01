19fc57d5010e4c8a33b187996a39442970b138c7
Kimiaki Kuno <knokmki612@gmail.com>
2022-04-01 09:26:50 +0900

    chore: ncu -u & setup react v18
    react                              ^17.0.2  →   ^18.0.0
    react-dom                          ^17.0.2  →   ^18.0.0
    @types/react                      ^17.0.41  →  ^17.0.43
    @types/webextension-polyfill        ^0.8.2  →    ^0.8.3
    @typescript-eslint/eslint-plugin    5.16.0  →    5.17.0
    @typescript-eslint/parser           5.16.0  →    5.17.0
    @vitejs/plugin-react                ^1.2.0  →    ^1.3.0
    eslint                              8.11.0  →    8.12.0
    eslint-plugin-jest                  26.1.2  →    26.1.3
    eslint-plugin-react-hooks           ^4.3.0  →    ^4.4.0
    prettier                             2.6.0  →     2.6.1
    ts-jest                             27.1.3  →    27.1.4
    typescript                           4.6.2  →     4.6.3
    vite                                ^2.8.6  →    ^2.9.0
    web-ext                             ^6.7.0  →    ^6.8.0
    webextension-polyfill               ^0.8.0  →    ^0.9.0

--- web-ext/src/main.tsx
+++ web-ext/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import "./style.css";
 import App from "./App";
 
-ReactDOM.render(
+// NOTE: @types/react-dom が v18 対応したら取り除いて
+// @ts-expect-error TS2339: Property 'createRoot' does not exist
+ReactDOM.createRoot(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
