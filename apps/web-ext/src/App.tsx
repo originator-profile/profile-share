a0e821c05c60f6743e38e3150990ce3777d79800
Kimiaki Kuno <knokmki612@gmail.com>
2022-04-06 18:40:09 +0900

    feat: UIライブラリを組み合わせたサンプル

--- apps/web-ext/src/App.tsx
+++ apps/web-ext/src/App.tsx
@@ -1,5 +1,23 @@
+import { InlineIcon } from "@iconify/react";
+import { Disclosure } from "@headlessui/react";
+
 function App(): React.ReactElement {
-  return <p>Hello World</p>;
+  return (
+    <div className="p-4">
+      <p css={{ letterSpacing: 2 }} className="mb-2">
+        Hello World <InlineIcon icon="heroicons-solid:heart" />
+      </p>
+      <button className="mb-2 jumpu-button">ボタン</button>
+      <Disclosure>
+        <div className="jumpu-accordion">
+          <Disclosure.Button>アコーディオン</Disclosure.Button>
+          <Disclosure.Panel className="p-2">
+            アコーディオンパネル
+          </Disclosure.Panel>
+        </div>
+      </Disclosure>
+    </div>
+  );
 }
 
 export default App;
