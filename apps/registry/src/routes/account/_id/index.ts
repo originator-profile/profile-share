a6878eb11a6c507b20effc5ae853abac5806fe4a
Kohei Watanabe <kou029w@gmail.com>
2022-04-14 14:40:14 +0900

    refactor: omit params type

--- apps/registry/src/routes/account/_id/index.ts
+++ apps/registry/src/routes/account/_id/index.ts
@@ -1,11 +1,16 @@
 import { FastifyInstance } from "fastify";
 import { FromHandler } from "../../../types";
+import Params from "./params";
 import getKeys from "./get-keys";
 import getProfiles from "./get-profiles";
 
 async function index(fastify: FastifyInstance): Promise<void> {
-  fastify.get<FromHandler<typeof getKeys>>("/keys", { ...getKeys }, getKeys);
-  fastify.get<FromHandler<typeof getProfiles>>(
+  fastify.get<FromHandler<typeof getKeys, Params>>(
+    "/keys",
+    { ...getKeys },
+    getKeys
+  );
+  fastify.get<FromHandler<typeof getProfiles, Params>>(
     "/profiles",
     { ...getProfiles },
     getProfiles
--- apps/registry/src/types.d.ts
+++ apps/registry/src/types.d.ts
@@ -8,12 +8,10 @@ declare module "fastify" {
   }
 }
 
-/** RouteHandlerMethod から RouteGenericInterface への型の変換 */
-type FromHandler<Handler> = Handler extends RouteHandlerMethod<
-  never,
-  never,
-  never,
-  infer RouteGeneric
->
-  ? RouteGeneric
+/** RouteHandlerMethod から RouteGeneric への型の変換 */
+type FromHandler<
+  Handler,
+  Params = unknown
+> = Handler extends RouteHandlerMethod<never, never, never, infer RouteGeneric>
+  ? Omit<RouteGeneric, "Params"> & { Params: Params }
   : never;
