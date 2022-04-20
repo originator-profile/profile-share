fc1ae92a2d5ca0a10a77cb37d6ebca0c7838fd20
Kohei Watanabe <kou029w@gmail.com>
2022-04-20 15:20:02 +0900

    OPの認証コマンドの実装

---
+++ apps/registry/src/commands/key-gen/index.ts
@@ -0,0 +1,21 @@
+import { Command, Flags } from "@oclif/core";
+import fs from "node:fs/promises";
+import { generateKey } from "@webdino/profile-sign";
+
+export class KeyGen extends Command {
+  static description = "鍵ペアの生成";
+  static flags = {
+    output: Flags.string({
+      char: "o",
+      description: "秘密鍵の保存先",
+      required: true,
+    }),
+  };
+
+  async run(): Promise<void> {
+    const { flags } = await this.parse(KeyGen);
+    const { jwk, pkcs8 } = await generateKey();
+    fs.writeFile(`${flags.output}.pub.json`, JSON.stringify(jwk));
+    fs.writeFile(flags.output, pkcs8);
+  }
+}
