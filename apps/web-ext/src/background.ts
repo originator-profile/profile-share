c36155a1326c8162b69ae3d00d94f3c08f94171e
Kimiaki Kuno <knokmki612@gmail.com>
2022-04-21 14:15:02 +0900

    feat(web-ext): 生成されるウィンドウのハンドリング

--- apps/web-ext/src/background.ts
+++ apps/web-ext/src/background.ts
@@ -1,6 +1,18 @@
 import browser from "webextension-polyfill";
 
-browser.browserAction.onClicked.addListener(() => {
-  const url = browser.runtime.getURL("index.html");
-  browser.windows.create({ url, type: "popup", width: 320, height: 640 });
+let windowId: number;
+
+browser.browserAction.onClicked.addListener(async function () {
+  try {
+    await browser.windows.update(windowId, { focused: true });
+  } catch {
+    const url = browser.runtime.getURL("index.html");
+    const window = await browser.windows.create({
+      url,
+      type: "popup",
+      width: 320,
+      height: 640,
+    });
+    windowId = window.id ?? NaN;
+  }
 });
