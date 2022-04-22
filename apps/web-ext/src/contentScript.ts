13ae92296a84f1d533a1bf9c9b3f5e1e6a551f18
Kimiaki Kuno <knokmki612@gmail.com>
2022-04-22 19:26:01 +0900

    feat: 拡張機能でのOP取得検証の実験的実装

---
+++ apps/web-ext/src/contentScript.ts
@@ -0,0 +1,29 @@
+import { createRemoteJWKSet, jwtVerify } from "jose";
+import { expand } from "jsonld";
+
+async function main() {
+  const context = "https://github.com/webdino/profile#";
+  const issuer = document.location.origin;
+  const jwksEndpoint = new URL(`${issuer}/.well-known/jwks.json`);
+  const targetOrigin = document.location.hash.slice(1) || issuer;
+  const opEndpoint = new URL(`${targetOrigin}/.well-known/op-document`);
+  const data = await fetch(opEndpoint.href)
+    .then((res) => res.json())
+    .catch((e) => e);
+  const [op] = await expand(data);
+  // @ts-expect-error assert
+  const profile: string[] = op[`${context}profile`].map(
+    (o: { "@value": string }) => o["@value"]
+  );
+  const jwks = createRemoteJWKSet(jwksEndpoint);
+  const verifies = await Promise.all(
+    profile.map((jwt: string) =>
+      jwtVerify(jwt, jwks, { issuer })
+        .then((dec) => dec.payload)
+        .catch((e) => e)
+    )
+  );
+  alert(JSON.stringify(verifies, null, 2));
+}
+
+main();
