1a88dff9161d31c41c788ff8ca9114b4c8c9045b
Kimiaki Kuno <knokmki612@gmail.com>
2022-04-28 19:05:04 +0900

    refactor: 不要な変更のrevert

--- apps/web-ext/src/pages/Holders.tsx
+++ apps/web-ext/src/pages/Holders.tsx
@@ -1,9 +1,9 @@
-import useOpDocumentsAtom from "../store/useOpsAtom";
+import useOpsAtom from "../store/useOpsAtom";
 import Spinner from "../components/Spinner";
 import ProfileItem from "../components/ProfileItem";
 
 function Holders(): React.ReactElement {
-  const { ops } = useOpDocumentsAtom();
+  const { ops } = useOpsAtom();
   if (!ops)
     return (
       <div className="w-screen h-screen flex justify-center items-center">
