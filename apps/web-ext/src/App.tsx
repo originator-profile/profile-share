2e0d7fc2119ff8bb5c3d5b6dc922d0947c498a7a
Kimiaki Kuno <knokmki612@gmail.com>
2022-04-28 16:13:15 +0900

    feat: 仮の見た目を取り除く

--- apps/web-ext/src/App.tsx
+++ apps/web-ext/src/App.tsx
@@ -1,6 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import { InlineIcon } from "@iconify/react";
-import { Disclosure } from "@headlessui/react";
 import Holders from "./pages/Holders";
 import Holder from "./pages/Holder";
 import Credential from "./pages/Credential";
@@ -8,29 +6,12 @@ import TechnicalInformation from "./pages/TechnicalInformation";
 
 function App(): React.ReactElement {
   return (
-    <div className="p-4">
-      <p css={{ letterSpacing: 2 }} className="mb-2">
-        Hello World <InlineIcon icon="heroicons-solid:heart" />
-      </p>
-      <button className="mb-2 jumpu-button">ボタン</button>
-      <Disclosure>
-        <div className="jumpu-accordion">
-          <Disclosure.Button>アコーディオン</Disclosure.Button>
-          <Disclosure.Panel className="p-2">
-            アコーディオンパネル
-          </Disclosure.Panel>
-        </div>
-      </Disclosure>
-      <Routes>
-        <Route path="/" element={<Holders />} />
-        <Route path="/holder" element={<Holder />} />
-        <Route path="/credential" element={<Credential />} />
-        <Route
-          path="/technical-information"
-          element={<TechnicalInformation />}
-        />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Holders />} />
+      <Route path="/holder/:subject" element={<Holder />} />
+      <Route path="/credential" element={<Credential />} />
+      <Route path="/technical-information" element={<TechnicalInformation />} />
+    </Routes>
   );
 }
 
