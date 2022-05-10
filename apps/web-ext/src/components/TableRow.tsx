211124e60498ef7f0c1495b602851837bddcd518
Kimiaki Kuno <knokmki612@gmail.com>
2022-05-10 12:16:55 +0900

    feat: 所有者情報を一覧するテーブルの実装

---
+++ apps/web-ext/src/components/TableRow.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  header: React.ReactNode;
+  data: React.ReactNode;
+};
+
+function TableRow({ header, data }: Props): React.ReactElement {
+  return (
+    <tr>
+      <th className="w-2/6 text-left pl-3 pr-1 py-2 text-gray-500 font-normal border-gray-200 border-b">
+        {header}
+      </th>
+      <td className="pr-3 py-2 border-gray-200 border-b">{data}</td>
+    </tr>
+  );
+}
+
+export default TableRow;
