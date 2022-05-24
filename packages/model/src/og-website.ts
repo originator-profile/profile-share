926bb81ce8f6634ed8b058b357712b2e97d393e1
Kohei Watanabe <kou029w@gmail.com>
2022-05-24 13:58:39 +0900

    DPの情報ジャンル/編集責任者/記事執筆者対応

--- packages/model/src/og-website.ts
+++ packages/model/src/og-website.ts
@@ -11,6 +11,9 @@ const OgWebsite = {
     title: { title: "Title", type: "string" },
     image: { title: "Image URL", type: "string" },
     description: { title: "Description", type: "string" },
+    "https://schema.org/author": { title: "Author", type: "string" },
+    "https://schema.org/category": { title: "Category", type: "string" },
+    "https://schema.org/editor": { title: "Editor", type: "string" },
   },
   required: ["type"],
 } as const;
