111a7e58f190c7e86585cf363cbc3befabad971f
Kohei Watanabe <kou029w@gmail.com>
2022-05-24 16:34:59 +0900

    fix: ウェブページ登録に失敗する問題の修正

--- apps/registry/src/seed.ts
+++ apps/registry/src/seed.ts
@@ -4,6 +4,7 @@ import crypto from "node:crypto";
 import { Services } from "@webdino/profile-registry-service";
 import { generateKey } from "@webdino/profile-sign";
 import exampleAccount from "./account.example.json";
+import exampleWebsite from "./website.example.json";
 
 export async function waitForDb(prisma: PrismaClient): Promise<void> {
   const sleep = util.promisify(setTimeout);
@@ -43,23 +44,33 @@ export async function seed(): Promise<void> {
   console.log(`UUID: ${issuerUuid}`);
 
   const accountKeys = await services.account.getKeys(issuerUuid);
-  if (accountKeys instanceof Error || accountKeys.keys.length === 0) {
-    const { jwk, pkcs8 } = await generateKey();
-    const data = await services.account.registerKey(issuerUuid, jwk);
-    if (data instanceof Error) throw data;
-    const jwt = await services.certificate.signOp(
-      issuerUuid,
-      issuerUuid,
-      pkcs8
-    );
-    if (jwt instanceof Error) throw jwt;
-    const opId = await services.certificate.issue(issuerUuid, jwt);
-    if (opId instanceof Error) throw opId;
-    await services.account.publishProfile(issuerUuid, opId);
-    console.log(`Profile: ${jwt}
+  if ("keys" in accountKeys && accountKeys.keys.length > 0) return;
+
+  const { jwk, pkcs8 } = await generateKey();
+  const data = await services.account.registerKey(issuerUuid, jwk);
+  if (data instanceof Error) throw data;
+  const jwt = await services.certificate.signOp(issuerUuid, issuerUuid, pkcs8);
+  if (jwt instanceof Error) throw jwt;
+  const opId = await services.certificate.issue(issuerUuid, jwt);
+  if (opId instanceof Error) throw opId;
+  await services.account.publishProfile(issuerUuid, opId);
+  console.log(`Profile: ${jwt}
 Public Key: ${JSON.stringify(jwk)}
 
 ${pkcs8}`);
+
+  const websiteExists = await prisma.websites.count({
+    where: { accountId: issuerUuid },
+  });
+  if (websiteExists === 0) {
+    const jwt = await services.publisher.registerWebsite(issuerUuid, pkcs8, {
+      ...exampleWebsite,
+      bodyFormat: exampleWebsite.bodyFormat as "html" | "text" | "visibleText",
+      website: { ...exampleWebsite.website, type: "website" },
+    });
+    if (jwt instanceof Error) throw jwt;
+    await services.publisher.issueDp(issuerUuid, jwt);
+    console.log(`Document Profile: ${jwt}`);
   }
 }
 
