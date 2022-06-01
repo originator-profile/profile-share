04203156f5828f0749fc08ba26b2379c8c159efd
Kimiaki Kuno <knokmki612@gmail.com>
2022-06-01 18:38:54 +0900

    fix: typo

--- apps/web-ext/src/utils/role.ts
+++ apps/web-ext/src/utils/role.ts
@@ -3,9 +3,9 @@ import { Role } from "../types/role";
 export const toRoles = (
   subject: string,
   advertisers: string[],
-  publisher: string[]
+  publishers: string[]
 ): Role[] =>
   [
     advertisers.includes(subject) ? ["advertiser" as const] : [],
-    publisher.includes(subject) ? ["publisher" as const] : [],
+    publishers.includes(subject) ? ["publisher" as const] : [],
   ].flat();
