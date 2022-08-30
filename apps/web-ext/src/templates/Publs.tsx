f8e12c42ed569440da74669ae3c83a4df7c02075
Kimiaki Kuno <knokmki612@gmail.com>
2022-08-30 09:43:09 +0900

    feat: Dp セレクター

---
+++ apps/web-ext/src/templates/Publs.tsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { Profile } from "../types/profile";
+import { isOp, isDp, isOpHolder } from "@webdino/profile-core";
+import { sortProfiles } from "../utils/profile";
+import { routes } from "../utils/routes";
+import Image from "../components/Image";
+
+type Props = {
+  profiles: Profile[];
+  main: string[];
+};
+
+function Publs({ profiles, main }: Props) {
+  return (
+    <ul>
+      {sortProfiles(profiles.filter(isDp), main).map((dp) => {
+        const op = profiles
+          .filter(isOp)
+          .find(({ subject }) => subject === dp.issuer);
+        const holder = op?.item.find(isOpHolder);
+        const logo = holder?.logos?.find(({ isMain }) => isMain);
+        return (
+          <li
+            key={`${encodeURIComponent(dp.issuer)}/${encodeURIComponent(
+              dp.subject
+            )}`}
+          >
+            <Link
+              className="flex justify-center items-center h-20 hover:bg-blue-50"
+              to={routes.publ.build(dp)}
+            >
+              <Image
+                src={logo?.url}
+                placeholderSrc="/assets/placeholder-logo-main.png"
+                alt={`${holder?.name}のロゴ`}
+                width={54}
+                height={54}
+                rounded
+              />
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Publs;
