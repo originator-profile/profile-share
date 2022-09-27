90be5be868eb703aec92a97fe23647d9b8bac901
Kimiaki Kuno <knokmki612@gmail.com>
2022-09-27 10:36:24 +0900

    feat: マーカーの実装

--- apps/web-ext/src/background.ts
+++ apps/web-ext/src/background.ts
@@ -1,3 +1,5 @@
+import { BackgroundMessageRequest } from "./types/message";
+
 const windowSize = {
   width: 520,
   height: 640,
@@ -7,6 +9,14 @@ chrome.action.onClicked.addListener(async function (tab) {
   const url = `${chrome.runtime.getURL("index.html")}#/tab/${tab.id}`;
   await chrome.windows.create({ url, type: "popup", ...windowSize });
 });
+
+const handleMessageResponse = async (message: BackgroundMessageRequest) => {
+  switch (message.type) {
+    case "select-overlay-dp":
+      await chrome.runtime.sendMessage(message);
+  }
+};
+chrome.runtime.onMessage.addListener(handleMessageResponse);
 
 // https://www.typescriptlang.org/tsconfig#non-module-files
 export {};
