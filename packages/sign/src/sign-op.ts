0701c2898d42ee13b706d48d0b03757c04e8bd0b
Kohei Watanabe <kou029w@gmail.com>
2022-09-28 14:31:14 +0900

    feat: `kid` 対応

--- packages/sign/src/sign-op.ts
+++ packages/sign/src/sign-op.ts
@@ -1,6 +1,7 @@
 import { importPKCS8, SignJWT } from "jose";
 import { getUnixTime } from "date-fns";
 import { Op, JwtOpPayload } from "@webdino/profile-model";
+import { fingerprint } from "./fingerprint";
 
 /**
  * OP への署名
@@ -16,6 +17,7 @@ export async function signOp(
 ): Promise<string> {
   const header = {
     alg,
+    kid: fingerprint(pkcs8),
     typ: "JWT",
   };
   const payload: Pick<JwtOpPayload, "https://opr.webdino.org/jwt/claims/op"> = {
