5023a74c410bcbcea1e04deed659a66c3e3d0cc7
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-05 16:21:14 +0900

    refactor: 型の整理

--- apps/web-ext/src/types/profile.ts
+++ apps/web-ext/src/types/profile.ts
@@ -1,5 +1,12 @@
-import { Dp as DpModel, Op as OpModel } from "@webdino/profile-model";
+import {
+  Dp as DpModel,
+  Op as OpModel,
+  DpText,
+  DpVisibleText,
+  DpHtml,
+} from "@webdino/profile-model";
 
 export type Op = OpModel & { error?: Error };
 export type Dp = DpModel & { error?: Error };
 export type Profile = Op | Dp;
+export type DpLocator = DpText | DpVisibleText | DpHtml;
