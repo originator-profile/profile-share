2787d3c9bdc468fb9a9bc3e0425cab0d19afc427
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-06 16:03:59 +0900

    feat: visibleText / text / html 型の検証時 url を確認するように

--- packages/verify/src/extract-body.ts
+++ packages/verify/src/extract-body.ts
@@ -1,8 +1,9 @@
 import { DpVisibleText, DpText, DpHtml } from "@webdino/profile-model";
+import { ProfileBodyExtractFailed } from "./errors";
 
-type Type = (DpVisibleText | DpText | DpHtml)["type"];
+type DpItem = DpVisibleText | DpText | DpHtml;
 
-function extract(element: HTMLElement, type: Type): string {
+function extract(element: HTMLElement, type: DpItem["type"]): string {
   switch (type) {
     case "visibleText":
       return element.innerText;
@@ -13,12 +14,19 @@ function extract(element: HTMLElement, type: Type): string {
   }
 }
 
-/** 対象の要素とその子孫にあたる文字列の抽出 */
-export function extractBody(
-  elements: NodeListOf<HTMLElement>,
-  type: Type
-): string {
+/**
+ * 対象の要素とその子孫にあたる文字列の抽出
+ * @param document Document インターフェイス
+ * @param item dp クレームの item プロパティの visibleText 型あるいは text 型あるいは html 型オブジェクト
+ * @returns 抽出した文字列
+ */
+export function extractBody(document: Document, item: DpItem) {
+  if (!document.location.href.startsWith(item.url))
+    return new ProfileBodyExtractFailed("Url mismatch");
+  const elements = document.querySelectorAll<HTMLElement>(
+    item.location ?? ":root"
+  );
   return Array.from(elements)
-    .map((element) => extract(element, type))
+    .map((element) => extract(element, item.type))
     .join("");
 }
