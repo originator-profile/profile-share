0d167a815bda64ba5386a25eab5d1c8b62310af0
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-06 16:03:59 +0900

    feat: visibleText / text / html 型の検証時 url を確認するように

--- apps/web-ext/src/utils/use-verify-body.ts
+++ apps/web-ext/src/utils/use-verify-body.ts
@@ -1,26 +1,25 @@
 import useSWRImmutable from "swr/immutable";
-import { verifyBody, LocalKeys, extractBody } from "@webdino/profile-verify";
+import {
+  verifyBody,
+  LocalKeys,
+  extractBody,
+  ProfileBodyExtractFailed,
+} from "@webdino/profile-verify";
 import { Jwks } from "@webdino/profile-model";
 import { DpLocator } from "../types/profile";
-import useElements from "../utils/use-elements";
 
 const key = "verify-body" as const;
 
-async function fetcher(
-  _: typeof key,
-  dpLocator: DpLocator,
-  elements: NodeListOf<HTMLElement>,
-  jwks?: Jwks
-) {
-  const body = extractBody(elements, dpLocator.type);
+async function fetcher(_: typeof key, dpLocator: DpLocator, jwks?: Jwks) {
+  const body = extractBody(window.parent.document, dpLocator);
+  if (body instanceof Error) return body;
   return verifyBody(body, dpLocator.proof.jws, LocalKeys(jwks ?? { keys: [] }));
 }
 
 function useVerifyBody(dpLocator: DpLocator, jwks?: Jwks) {
-  const { elements } = useElements(dpLocator.location);
   const { data: result, error } = useSWRImmutable<
-    Awaited<ReturnType<typeof verifyBody>>
-  >([key, dpLocator, elements, jwks], fetcher);
+    Awaited<ReturnType<typeof verifyBody>> | ProfileBodyExtractFailed
+  >([key, dpLocator, jwks], fetcher);
   return { result, error };
 }
 
