2e27073ca5f8b1b55ea2bd124c51e1bcef471991
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-24 16:07:58 +0900

    fix: lint error

--- apps/web-ext/src/utils/profile.ts
+++ apps/web-ext/src/utils/profile.ts
@@ -21,8 +21,8 @@ export const toProfile = (verifyResult: VerifyResult): Profile => {
 
 const reorder = <T>(list: T[], startIndex: number, endIndex: number) => {
   const result = [...list];
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
+  const removed = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, ...removed);
   return result;
 };
 
