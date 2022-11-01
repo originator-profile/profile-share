9fb5c153109ee5551a2fd48a28cee39b5bf08efe
Kimiaki Kuno <knokmki612@gmail.com>
2022-11-01 09:57:11 +0900

    feat: startTransition のタイミングで座標を更新するように

--- apps/web-ext/src/utils/use-rects.ts
+++ apps/web-ext/src/utils/use-rects.ts
@@ -1,23 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, startTransition } from "react";
 import { useEvent } from "react-use";
-import { useThrottledCallback } from "use-debounce";
 import useIntersectingElements from "./use-intersecting-elements";
 
 /** 要素の寸法と相対位置を返すフック関数 */
 function useRects(elements: NodeListOf<HTMLElement>) {
   const { intersectingElements } = useIntersectingElements(elements);
   const [rects, setRects] = useState<DOMRect[]>([]);
-  const handler = useThrottledCallback(() => {
-    setRects(
-      intersectingElements.map((element) => element.getBoundingClientRect())
-    );
-  }, 10);
+  const handler = () => {
+    startTransition(() => {
+      setRects(
+        intersectingElements.map((intersectingElement) =>
+          intersectingElement.getBoundingClientRect()
+        )
+      );
+    });
+  };
   useEvent("resize", handler, window.parent);
   useEvent("scroll", handler, window.parent);
   useEffect(() => {
-    handler();
-    // NOTE: オーバーレイの初期状態（リサイズ、スクロール前）でマーカーが表示されないことへの対処
-  }, [handler, intersectingElements]);
+    startTransition(() => {
+      setRects(
+        intersectingElements.map((intersectingElement) =>
+          intersectingElement.getBoundingClientRect()
+        )
+      );
+    });
+  }, [setRects, intersectingElements]);
   return { rects };
 }
 
