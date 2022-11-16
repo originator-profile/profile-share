cbc2f8012f6cf5f8a030381a795aac6a85977110
Kimiaki Kuno <knokmki612@gmail.com>
2022-11-16 21:19:04 +0900

    feat: useElements で複数のロケーションを受け付けるように

--- apps/web-ext/src/utils/use-elements.ts
+++ apps/web-ext/src/utils/use-elements.ts
@@ -1,9 +1,14 @@
 import { useMemo } from "react";
 
 /** CSS セレクターで指定した要素を返すフック関数 */
-function useElements(location?: string) {
+function useElements(location?: string | (string | undefined)[]) {
   const elements = useMemo<NodeListOf<HTMLElement>>(
-    () => window.parent.document.querySelectorAll(location ?? ":root"),
+    () =>
+      window.parent.document.querySelectorAll(
+        (location instanceof Array
+          ? location.filter(Boolean).join(", ")
+          : location) || ":root"
+      ),
     [location]
   );
   return { elements };
