2fedaf40a0a0e55fe7e078ff8423e09bf8c07a69
Kimiaki Kuno <knokmki612@gmail.com>
2023-02-22 15:16:32 +0900

    fix: e2e テストが同時に複数実行された場合にtempファイルが競合する

--- apps/registry/e2e/publisher.test.ts
+++ apps/registry/e2e/publisher.test.ts
@@ -5,26 +5,32 @@ import fs from "node:fs/promises";
 import { existsSync } from "node:fs";
 import path from "node:path";
 
-const tmpdir = path.join(__dirname, "tmp");
-const paths = {
-  extract: path.join(tmpdir, "extract.json"),
-  key: path.join(__dirname, "..", "account-key.example.key"),
-} as const;
+const paths = (tmpdir: string) =>
+  ({
+    extract: path.join(tmpdir, "extract.json"),
+    key: path.join(__dirname, "..", "account-key.example.key"),
+  } as const);
 const glob = "tmp/**/website.json";
 
-const extract = {
-  url: "http://localhost:8080",
-  bodyFormat: "visibleText",
-  location: "h1",
-  output: path.join(tmpdir, "website.json"),
-} as const;
+const extract = (tmpdir: string) =>
+  ({
+    url: "http://localhost:8080",
+    bodyFormat: "visibleText",
+    location: "h1",
+    output: path.join(tmpdir, "website.json"),
+  } as const);
 
 const accountId = "d613c1d6-5312-41e9-98ad-2b99765955b6";
 
 describe("Publisher workflows", async () => {
+  let tmpdir: string;
+
   beforeAll(async () => {
-    await fs.mkdir(tmpdir);
-    await fs.writeFile(paths.extract, JSON.stringify([extract], null, 2));
+    tmpdir = await fs.mkdtemp(path.join(__dirname, "tmp"));
+    await fs.writeFile(
+      paths(tmpdir).extract,
+      JSON.stringify([extract(tmpdir)], null, 2)
+    );
   });
 
   afterAll(async () => {
@@ -32,17 +38,17 @@ describe("Publisher workflows", async () => {
   });
 
   test("Website extraction makes website.json", async () => {
-    await PublisherExtractWebsite.run([`--input=${paths.extract}`]);
-    expect(existsSync(extract.output)).toBe(true);
+    await PublisherExtractWebsite.run([`--input=${paths(tmpdir).extract}`]);
+    expect(existsSync(extract(tmpdir).output)).toBe(true);
     const website = await fs
-      .readFile(extract.output)
+      .readFile(extract(tmpdir).output)
       .then((buffer) => JSON.parse(buffer.toString()));
     expect(website.body).toBe("OP 確認くん");
   });
 
   test("Website updation to be success", async () => {
     await PublisherWebsite.run([
-      `--identity=${paths.key}`,
+      `--identity=${paths(tmpdir).key}`,
       `--id=${accountId}`,
       "--operation=update",
       `--glob-input=${glob}`,
