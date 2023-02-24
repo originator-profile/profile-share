6280f6958ccef4abe976cf84c06c224a391e5019
Kohei Watanabe <kou029w@gmail.com>
2023-02-24 16:13:00 +0900

    test: website.jsonによる更新処理のテストの整理
    - `account-key.example.key` → `account-key.example.pem` 参照されていないので更新
    - `describe()` によるグループ化
      - 関数呼び出し → `let` を使う
    - `website.json` のパスの参照に失敗している問題の修正
      - `tmpXXXXXX` ディレクトリではなく `tmp` ディレクトリ以下が参照されている問題があったため修正
      - `tmpXXXXXX` → `tmp/XXXXXX` ディレクトリに変更
      - `website.json` 相対パス→絶対パスを使う
      - `website.json` のパスの非glob化

--- apps/registry/e2e/publisher.test.ts
+++ apps/registry/e2e/publisher.test.ts
@@ -1,35 +1,31 @@
 import { afterAll, beforeAll, describe, test, expect } from "vitest";
+import fs from "node:fs/promises";
+import path from "node:path";
 import { PublisherExtractWebsite } from "../src/commands/publisher/extract-website";
 import { PublisherWebsite } from "../src/commands/publisher/website";
-import fs from "node:fs/promises";
-import { existsSync } from "node:fs";
-import path from "node:path";
-
-const paths = (tmpdir: string) =>
-  ({
-    extract: path.join(tmpdir, "extract.json"),
-    key: path.join(__dirname, "..", "account-key.example.key"),
-  } as const);
-const glob = "tmp/**/website.json";
-
-const extract = (tmpdir: string) =>
-  ({
-    url: "http://localhost:8080",
-    bodyFormat: "visibleText",
-    location: "h1",
-    output: path.join(tmpdir, "website.json"),
-  } as const);
 
 const accountId = "d613c1d6-5312-41e9-98ad-2b99765955b6";
+const keyPath = path.join(__dirname, "..", "account-key.example.pem");
 
 describe("Publisher workflows", async () => {
   let tmpdir: string;
+  let extractJsonPath: string;
+  let websiteJsonPath: string;
 
   beforeAll(async () => {
-    tmpdir = await fs.mkdtemp(path.join(__dirname, "tmp"));
+    tmpdir = await fs.mkdtemp(`${path.join(__dirname, "tmp")}${path.sep}`);
+    extractJsonPath = path.join(tmpdir, "extract.json");
+    websiteJsonPath = path.join(tmpdir, "website.json");
     await fs.writeFile(
-      paths(tmpdir).extract,
-      JSON.stringify([extract(tmpdir)], null, 2)
+      extractJsonPath,
+      JSON.stringify([
+        {
+          url: "http://localhost:8080",
+          bodyFormat: "visibleText",
+          location: "h1",
+          output: websiteJsonPath,
+        },
+      ])
     );
   });
 
@@ -38,20 +34,19 @@ describe("Publisher workflows", async () => {
   });
 
   test("Website extraction makes website.json", async () => {
-    await PublisherExtractWebsite.run([`--input=${paths(tmpdir).extract}`]);
-    expect(existsSync(extract(tmpdir).output)).toBe(true);
+    await PublisherExtractWebsite.run([`--input=${extractJsonPath}`]);
     const website = await fs
-      .readFile(extract(tmpdir).output)
+      .readFile(websiteJsonPath)
       .then((buffer) => JSON.parse(buffer.toString()));
     expect(website.body).toBe("OP 確認くん");
   });
 
-  test("Website updation to be success", async () => {
+  test("Update from website.json should succeed", async () => {
     await PublisherWebsite.run([
-      `--identity=${paths(tmpdir).key}`,
+      `--identity=${keyPath}`,
       `--id=${accountId}`,
       "--operation=update",
-      `--glob-input=${glob}`,
+      `--glob-input=${websiteJsonPath}`,
     ]);
   });
 });
