dfeb14ab11c331192c8358c033fc9ec9632239a8
Kohei Watanabe <kou029w@gmail.com>
2023-02-24 16:13:12 +0900

    feat: `--glob-input` の絶対パス対応

--- apps/registry/src/commands/publisher/website.ts
+++ apps/registry/src/commands/publisher/website.ts
@@ -3,7 +3,6 @@ import { PrismaClient, Prisma } from "@prisma/client";
 import { addYears } from "date-fns";
 import { Services } from "@webdino/profile-registry-service";
 import fs from "node:fs/promises";
-import { join } from "node:path";
 import { globby } from "globby";
 
 export class PublisherWebsite extends Command {
@@ -99,7 +98,7 @@ https://profile-docs.pages.dev/ts/modules/_webdino_profile_registry_db.default.P
       await this.#website(flags);
       return;
     }
-    const paths = await globby(join("**", flags["glob-input"]));
+    const paths = await globby(flags["glob-input"]);
     const bar = CliUx.ux.progress();
     bar.start(paths.length, 0);
     await Promise.all(
