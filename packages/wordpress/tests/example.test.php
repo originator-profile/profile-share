8b47379b6cda9de09bc96160b52cbbe47d1c9c17
Kohei Watanabe <kou029w@gmail.com>
2023-03-14 13:28:19 +0900

    feat: WordPress Profile Pluginの鍵の配置

--- packages/wordpress/tests/example.test.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-use PHPUnit\Framework\TestCase;
-
-final class Example extends TestCase {
-	public function test_1と2の合計は3です(): void {
-		$sum = 1 + 2;
-		$this->assertSame( $sum, 3 );
-	}
-}
