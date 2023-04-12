eb3a3aed9635c7efdca5cc0ef9f5abf63fbf6784
Kimiaki Kuno <knokmki612@gmail.com>
2023-04-12 11:04:02 +0900

    feat: OpCredential の拡張

--- packages/model/src/op-credential.ts
+++ packages/model/src/op-credential.ts
@@ -6,8 +6,42 @@ const OpCredential = {
   type: "object",
   properties: {
     type: { const: "credential" },
+    certifierId: {
+      title: "認証機関を表す一義的な識別子",
+      type: "string",
+    },
+    verifierId: {
+      title: "検証機関を表す一義的な識別子",
+      type: "string",
+    },
+    name: {
+      title: "資格名",
+      type: "string",
+    },
+    image: {
+      title: "画像URL",
+      type: "string",
+    },
+    issuedAt: {
+      title: "発行日時",
+      type: "string",
+      format: "date-time",
+    },
+    expiredAt: {
+      title: "有効期限",
+      type: "string",
+      format: "date-time",
+    },
   },
-  required: ["type"],
+  required: [
+    "type",
+    "certifierId",
+    "verifierId",
+    "name",
+    "issuedAt",
+    "expiredAt",
+  ],
+  additionalProperties: false,
 } as const;
 
 type OpCredential = FromSchema<typeof OpCredential>;
