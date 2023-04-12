08f6382addf5c559400f40f4cec839a4bca192a1
Kohei Watanabe <kou029w@gmail.com>
2023-04-12 18:42:15 +0900

    feat: 複数 <link> 要素対応
    閲覧中のウェブページに複数の <link> 要素が存在する場合のProfiles Setの取得に対応します。ただし仕様の中に明記されていないiframeなどの複数のコンテキストが存在するケースについては未対応です。

--- packages/verify/src/fetch-profiles.test.ts
+++ packages/verify/src/fetch-profiles.test.ts
@@ -1,6 +1,6 @@
 import "vi-fetch/setup";
 import { mockFetch, mockGet } from "vi-fetch";
-import { describe, beforeEach, test, expect } from "vitest";
+import { describe, beforeEach, afterEach, test, expect } from "vitest";
 import { Window } from "happy-dom";
 import { addYears, getUnixTime, fromUnixTime } from "date-fns";
 import { JsonLdDocument } from "jsonld";
@@ -30,8 +30,11 @@ describe("fetch-profiles", async () => {
   const profileEndpoint = "https://example.com/.well-known/ps.json";
 
   beforeEach(() => {
-    mockFetch.clearAll();
     mockGet(profileEndpoint).willResolve(profiles);
+  });
+
+  afterEach(() => {
+    mockFetch.clearAll();
   });
 
   test("有効なエンドポイント指定時 Profiles Set が得られる", async () => {
@@ -43,7 +46,7 @@ describe("fetch-profiles", async () => {
   type="application/ld+json"
 />`;
     const result = await fetchProfiles(window.document as unknown as Document);
-    expect(result).toEqual(profiles);
+    expect(result).toEqual([profiles]);
   });
 
   test("無効なエンドポイント指定時 Profiles Set の取得に失敗", async () => {
@@ -77,5 +80,43 @@ describe("fetch-profiles", async () => {
     expect(result.message).toBe(
       `プロファイルを取得できませんでした:\nHTTP ステータスコード 404`
     );
+  });
+
+  describe("<link> 要素が2つ以上存在するとき", async () => {
+    beforeEach(() => {
+      mockGet("https://example.com/1/ps.json").willResolve({
+        "@context": "https://oprdev.herokuapp.com/context",
+        profiles: "{Singed Document Profile または Singed Originator Profile}",
+      });
+      mockGet("https://example.com/2/ps.json").willResolve({
+        "@context": "https://oprdev.herokuapp.com/context",
+        profiles:
+          "{別の Singed Document Profile または Singed Originator Profile}",
+      });
+    });
+
+    test("有効な Profiles Set が得られる", async () => {
+      const window = new Window();
+      const profileEndpoints = [
+        "https://example.com/1/ps.json",
+        "https://example.com/2/ps.json",
+      ];
+      window.document.body.innerHTML = profileEndpoints
+        .map(
+          (endpoint) => `
+<link
+  href="${endpoint}"
+  rel="alternate"
+  type="application/ld+json"
+/>
+  `
+        )
+        .join("");
+      const result = await fetchProfiles(
+        window.document as unknown as Document
+      );
+      expect(result).not.toBeInstanceOf(ProfilesFetchFailed);
+      expect(result).toMatchSnapshot();
+    });
   });
 });
