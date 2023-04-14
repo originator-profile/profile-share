1290dd27161d3602188fe0f51804e924a650a834
Kohei Watanabe <kou029w@gmail.com>
2023-04-14 11:02:01 +0900

    fix: 複数のnodeが存在する場合の不具合の修正
    先頭以外展開されない不具合がありました。それを修正します。

--- packages/verify/src/expand-profiles.ts
+++ packages/verify/src/expand-profiles.ts
@@ -1,32 +1,47 @@
-import jsonld, { JsonLdDocument } from "jsonld";
+import jsonld, { JsonLdDocument, NodeObject } from "jsonld";
+
+function nodeToObj(node: NodeObject) {
+  const context = "https://github.com/webdino/profile#";
+  const advertisers: string[] =
+    // @ts-expect-error assert
+    node[`${context}advertiser`]?.map(
+      (advertiser: { "@value": string }) => advertiser["@value"]
+    ) ?? [];
+  const publishers: string[] =
+    // @ts-expect-error assert
+    node[`${context}publisher`]?.map(
+      (publisher: { "@value": string }) => publisher["@value"]
+    ) ?? [];
+  const main: string[] =
+    // @ts-expect-error assert
+    node[`${context}main`]?.map(
+      (main: { "@value": string }) => main["@value"]
+    ) ?? [];
+  // @ts-expect-error assert
+  const profile: string[] = node[`${context}profile`].map(
+    (profile: { "@value": string }) => profile["@value"]
+  );
+  return { advertisers, publishers, main, profile };
+}
 
 /**
  * Profiles Set の JSON-LD 表現の Profile Set への展開
  * @param profiles Profiles Set の JSON-LD 表現
  */
 export async function expandProfiles(profiles: JsonLdDocument) {
-  const [expanded] = await jsonld.expand(profiles);
-  const context = "https://github.com/webdino/profile#";
-  if (!expanded)
-    return { advertisers: [], publishers: [], main: [], profile: [] };
-  const advertisers: string[] =
-    // @ts-expect-error assert
-    expanded[`${context}advertiser`]?.map(
-      (advertiser: { "@value": string }) => advertiser["@value"]
-    ) ?? [];
-  const publishers: string[] =
-    // @ts-expect-error assert
-    expanded[`${context}publisher`]?.map(
-      (publisher: { "@value": string }) => publisher["@value"]
-    ) ?? [];
-  const main: string[] =
-    // @ts-expect-error assert
-    expanded[`${context}main`]?.map(
-      (main: { "@value": string }) => main["@value"]
-    ) ?? [];
-  // @ts-expect-error assert
-  const profile: string[] = expanded[`${context}profile`].map(
-    (profile: { "@value": string }) => profile["@value"]
-  );
-  return { advertisers, publishers, main, profile };
+  const expanded = await jsonld.expand(profiles);
+  const res: {
+    advertisers: string[];
+    publishers: string[];
+    main: string[];
+    profile: string[];
+  } = { advertisers: [], publishers: [], main: [], profile: [] };
+  for (const node of expanded) {
+    const obj = nodeToObj(node);
+    res.advertisers.push(...obj.advertisers);
+    res.publishers.push(...obj.publishers);
+    res.main.push(...obj.main);
+    res.profile.push(...obj.profile);
+  }
+  return res;
 }
