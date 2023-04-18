5145284a9116a85f945d59f597d85f75a8992654
Kimiaki Kuno <knokmki612@gmail.com>
2023-04-18 17:57:54 +0900

    feat: 資格保有者の概要の見た目の外部化

---
+++ apps/web-ext/src/components/HolderSummary.tsx
@@ -0,0 +1,95 @@
+import clsx from "clsx";
+import { Link, LinkProps } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import { OpHolder } from "@webdino/profile-model";
+import Image from "../components/Image";
+import placeholderLogoMainUrl from "../assets/placeholder-logo-main.png";
+import logoCertifierUrl from "../assets/logo-certifier.png";
+
+type Props = {
+  className?: string;
+  to: LinkProps["to"];
+  holder: OpHolder;
+};
+
+function HolderSummary({ className, to, holder }: Props) {
+  const logo = holder.logos?.find(({ isMain }) => isMain);
+  return (
+    <Link className={clsx("jumpu-card block", className)} to={to}>
+      <div className="flex items-center gap-4 mx-4 my-3">
+        <Image
+          className="flex-shrink-0"
+          src={logo?.url}
+          placeholderSrc={placeholderLogoMainUrl}
+          alt=""
+          width={60}
+          height={60}
+          rounded
+        />
+        <div>
+          <p className="text-gray-700 text-sm">この記事を発行した組織</p>
+          <p className="text-lg">{holder.name}</p>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 bg-blue-50 p-2 mx-4 my-3 rounded-sm">
+        <Icon
+          className="flex-shrink-0 text-blue-500 text-2xl"
+          icon="akar-icons:circle-check-fill"
+        />
+        <p className="flex-1 text-blue-500 text-base font-bold">
+          この組織は認証を受けています
+        </p>
+      </div>
+      <div className="flex mx-4 my-3 gap-4">
+        <Image
+          className="flex-shrink-0"
+          src={logoCertifierUrl}
+          placeholderSrc={placeholderLogoMainUrl}
+          alt=""
+          width={55}
+          height={35}
+        />
+        <div>
+          <p className="text-sm text-gray-500 mb-1">
+            <span className="font-bold text-gray-700 mr-1">
+              無効トラフィック対策認証 第三者検証
+            </span>
+            その他2件の認証情報
+          </p>
+          <p className="text-xs">
+            <Icon
+              className="inline text-blue-600 mr-1"
+              icon="akar-icons:check"
+            />
+            有効期限内
+          </p>
+        </div>
+      </div>
+      {holder.publishingPrincipleUrl && (
+        <a
+          className="block bg-gray-50 rounded-lg px-4 py-2 mx-4 my-3"
+          href={holder.publishingPrincipleUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="text-sm font-bold text-gray-600 mb-1">
+            編集ガイドライン
+          </p>
+          <p className="text-xs text-blue-600">
+            {holder.publishingPrincipleTitle ?? holder.publishingPrincipleUrl}
+            <Icon
+              className="inline ml-1 text-gray-500"
+              icon="fa-solid:external-link-alt"
+            />
+          </p>
+        </a>
+      )}
+      <div className="flex gap-2 justify-center items-center text-sm border-t boder-gray-100 px-4 py-3">
+        <span className="text-sm font-bold">組織情報を見る</span>
+        <Icon className="flex-shrink-0" icon="fa6-solid:chevron-right" />
+      </div>
+    </Link>
+  );
+}
+
+export default HolderSummary;
