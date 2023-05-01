7a844f15b1cf322fd2a8e865ac9bf13bfe09cfc9
Kimiaki Kuno <knokmki612@gmail.com>
2023-05-01 16:04:30 +0900

    chore: TailwindCSS の設定の外部化

---
+++ packages/tailwind-config/tailwind-config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    ...require("@jumpu-ui/tailwindcss"),
+    require("@tailwindcss/typography"),
+  ],
+};
