9aeb3287d21352abda6d2a2239aa4fa86a3fe450
MURATA, Shuuichirou <mrt@acutus.jp>
2023-05-19 18:49:27 +0900

    feat(verify): extractBodyのテストにおいてtext,html型もchromium,firefox,webkitで実施

--- packages/verify/src/extract-body.test.ts
+++ packages/verify/src/extract-body.test.ts
@@ -23,7 +23,7 @@ const pageUrl = document.location.href;
 const locator = async (location: string) =>
   Array.from(document.querySelectorAll<HTMLElement>(location));
 
-const extractTest = async function(page:Page) {
+const extractVisibleText = async function(page:Page) {
   const item: DpVisibleText = {
     ...base,
     type: "visibleText",
@@ -32,7 +32,7 @@ const extractTest = async function(page:Page) {
     el.innerHTML = html;
   }, html);
   const result = await extractBody(
-    item.url,
+    pageUrl,
     (location) => page.locator(location).all(),
     item
   );
@@ -43,49 +43,124 @@ const extractTest = async function(page:Page) {
 test("extract body as visibleText type on chromium", async () => {
   const browser = await chromium.launch();
   const context = await browser.newContext();
-  await extractTest(await context.newPage());
+  await extractVisibleText(await context.newPage());
 });
 
 test("extract body as visibleText type on firefox", async () => {
   const browser = await firefox.launch();
   const context = await browser.newContext();
-  await extractTest(await context.newPage());
+  await extractVisibleText(await context.newPage());
 });
 
 test("extract body as visibleText type on webkit", async () => {
   const browser = await webkit.launch();
   const context = await browser.newContext();
-  await extractTest(await context.newPage());
+  await extractVisibleText(await context.newPage());
 });
 
-test("extract body as text type", async () => {
+const extractText = async function(page:Page) {
   const item: DpText = {
     ...base,
     type: "text",
   };
-  const result = await extractBody(pageUrl, locator, item);
+  await page.locator("body").evaluate((el, html) => {
+    el.innerHTML = html;
+  }, html);
+  const result = await extractBody(
+    pageUrl,
+    (location) => page.locator(location).all(),
+    item
+  );
   expect(result).not.instanceOf(Error);
   expect(result).toBe("Hello, World!NoneGoodbye, World!");
+}
+
+test("extract body as text type on chromium", async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  await extractText(await context.newPage());
 });
 
-test("extract body as html type", async () => {
+test("extract body as text type on firefox", async () => {
+  const browser = await firefox.launch();
+  const context = await browser.newContext();
+  await extractText(await context.newPage());
+});
+
+test("extract body as text type on webkit", async () => {
+  const browser = await webkit.launch();
+  const context = await browser.newContext();
+  await extractText(await context.newPage());
+});
+
+const extractHtml = async function(page:Page) {
   const item: DpHtml = {
     ...base,
     type: "html",
   };
-  const result = await extractBody(pageUrl, locator, item);
+  await page.locator("body").evaluate((el, html) => {
+    el.innerHTML = html;
+  }, html);
+  const result = await extractBody(
+    pageUrl,
+    (location) => page.locator(location).all(),
+    item
+  );
   expect(result).not.instanceOf(Error);
   expect(result).toBe(
     '<body><p>Hello, World!</p><p style="display:none">None</p><p>Goodbye, World!</p></body>'
   );
+}
+
+test("extract body as html type on chromium", async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  await extractHtml(await context.newPage());
 });
 
-test("extract body failure when url misatch", async () => {
+test("extract body as html type on firefox", async () => {
+  const browser = await firefox.launch();
+  const context = await browser.newContext();
+  await extractHtml(await context.newPage());
+});
+
+test("extract body as html type on webkit", async () => {
+  const browser = await webkit.launch();
+  const context = await browser.newContext();
+  await extractHtml(await context.newPage());
+});
+
+const extractEvil = async function(page:Page) {
   const item: DpText = {
     ...base,
     url: "https://evil.com",
     type: "text",
   };
-  const result = await extractBody(pageUrl, locator, item);
+  await page.locator("body").evaluate((el, html) => {
+    el.innerHTML = html;
+  }, html);
+  const result = await extractBody(
+    pageUrl,
+    (location) => page.locator(location).all(),
+    item
+  );
   expect(result).instanceOf(Error);
+}
+
+test("extract body failure when url mismatch on chromium", async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  await extractEvil(await context.newPage());
+});
+
+test("extract body failure when url mismatch on firefox", async () => {
+  const browser = await firefox.launch();
+  const context = await browser.newContext();
+  await extractEvil(await context.newPage());
+});
+
+test("extract body failure when url mismatch on webkit", async () => {
+  const browser = await webkit.launch();
+  const context = await browser.newContext();
+  await extractEvil(await context.newPage());
 });
