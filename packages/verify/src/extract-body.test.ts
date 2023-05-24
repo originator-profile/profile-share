24750eb7796374593a327537262023a09744dbbc
Murata Shuuichirou <mrt@acutus.co.jp>
2023-05-24 22:29:04 +0900

    feat(verify): describe()を用いてブラウザごとにテストをグループ化

--- packages/verify/src/extract-body.test.ts
+++ packages/verify/src/extract-body.test.ts
@@ -1,8 +1,8 @@
-import { test, expect } from "vitest";
+import { describe, beforeAll, test, expect } from "vitest";
 import { Window } from "happy-dom";
 import { DpVisibleText, DpText, DpHtml } from "@webdino/profile-model";
 import { extractBody } from "./extract-body";
-import { chromium, firefox, webkit, Locator, Page } from "playwright";
+import { chromium, firefox, webkit, Locator } from "playwright";
 
 type Locale = HTMLElement | Locator;
 
@@ -17,48 +17,19 @@ const base = {
 
 const html =
   '<p>Hello, World!</p><p style="display:none">None</p><p>Goodbye, World!</p>';
-const window = new Window();
-const document = window.document as unknown as Document;
-document.body.innerHTML = html;
-document.location.href = "https://example.com/";
-const pageUrl = document.location.href;
-const locator = async (location: string) =>
-  Array.from(document.querySelectorAll<HTMLElement>(location));
+const pageUrl = "https://example.com/";
 
-const extractVisibleText = async function (page: Page) {
+const extractVisibleText = async function (
+  locator: (location: string) => Promise<Locale[]>
+) {
   const item: DpVisibleText = {
     ...base,
     type: "visibleText",
   };
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  const result = await extractBody(
-    pageUrl,
-    (location) => page.locator(location).all(),
-    item
-  );
+  const result = await extractBody(pageUrl, locator, item);
   expect(result).not.instanceOf(Error);
   expect(result).toBe("Hello, World!\n\nGoodbye, World!");
 };
-
-test("extract body as visibleText type on chromium", async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  await extractVisibleText(await context.newPage());
-});
-
-test("extract body as visibleText type on firefox", async () => {
-  const browser = await firefox.launch();
-  const context = await browser.newContext();
-  await extractVisibleText(await context.newPage());
-});
-
-test("extract body as visibleText type on webkit", async () => {
-  const browser = await webkit.launch();
-  const context = await browser.newContext();
-  await extractVisibleText(await context.newPage());
-});
 
 const extractText = async function (
   locator: (location: string) => Promise<Locale[]>
@@ -71,40 +42,6 @@ const extractText = async function (
   expect(result).not.instanceOf(Error);
   expect(result).toBe("Hello, World!NoneGoodbye, World!");
 };
-
-test("extract body as text type on happy-dom", async () => {
-  await extractText(locator);
-});
-
-test("extract body as text type on chromium", async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractText((location) => page.locator(location).all());
-});
-
-test("extract body as text type on firefox", async () => {
-  const browser = await firefox.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractText((location) => page.locator(location).all());
-});
-
-test("extract body as text type on webkit", async () => {
-  const browser = await webkit.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractText((location) => page.locator(location).all());
-});
 
 const extractHtml = async function (
   locator: (location: string) => Promise<Locale[]>
@@ -120,40 +57,6 @@ const extractHtml = async function (
   );
 };
 
-test("extract body as html type on happy-dom", async () => {
-  await extractHtml(locator);
-});
-
-test("extract body as html type on chromium", async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractHtml((location) => page.locator(location).all());
-});
-
-test("extract body as html type on firefox", async () => {
-  const browser = await firefox.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractHtml((location) => page.locator(location).all());
-});
-
-test("extract body as html type on webkit", async () => {
-  const browser = await webkit.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractHtml((location) => page.locator(location).all());
-});
-
 const extractEvil = async function (
   locator: (location: string) => Promise<Locale[]>
 ) {
@@ -166,36 +69,117 @@ const extractEvil = async function (
   expect(result).instanceOf(Error);
 };
 
-test("extract body failure when url mismatch on happy-dom", async () => {
-  await extractEvil(locator);
+describe("extract body on chromium", () => {
+  let locator: (location: string) => Promise<Locator[]>;
+
+  beforeAll(async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.locator("body").evaluate((el, html) => {
+      el.innerHTML = html;
+    }, html);
+    locator = (location: string) => page.locator(location).all();
+  });
+
+  test("as visibleText type", async () => {
+    await extractVisibleText(locator);
+  });
+
+  test("as text type", async () => {
+    await extractText(locator);
+  });
+
+  test("as html type", async () => {
+    await extractHtml(locator);
+  });
+
+  test("failure when url mismatch", async () => {
+    await extractEvil(locator);
+  });
 });
 
-test("extract body failure when url mismatch on chromium", async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractEvil((location) => page.locator(location).all());
+describe("extract body on firefox", () => {
+  let locator: (location: string) => Promise<Locator[]>;
+
+  beforeAll(async () => {
+    const browser = await firefox.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.locator("body").evaluate((el, html) => {
+      el.innerHTML = html;
+    }, html);
+    locator = (location: string) => page.locator(location).all();
+  });
+
+  test("as visibleText type", async () => {
+    await extractVisibleText(locator);
+  });
+
+  test("as text type", async () => {
+    await extractText(locator);
+  });
+
+  test("as html type", async () => {
+    await extractHtml(locator);
+  });
+
+  test("failure when url mismatch", async () => {
+    await extractEvil(locator);
+  });
 });
 
-test("extract body failure when url mismatch on firefox", async () => {
-  const browser = await firefox.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractEvil((location) => page.locator(location).all());
+describe("extract body on webkit", () => {
+  let locator: (location: string) => Promise<Locator[]>;
+
+  beforeAll(async () => {
+    const browser = await webkit.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.locator("body").evaluate((el, html) => {
+      el.innerHTML = html;
+    }, html);
+    locator = (location: string) => page.locator(location).all();
+  });
+
+  test("as visibleText type", async () => {
+    await extractVisibleText(locator);
+  });
+
+  test("as text type", async () => {
+    await extractText(locator);
+  });
+
+  test("as html type", async () => {
+    await extractHtml(locator);
+  });
+
+  test("failure when url mismatch", async () => {
+    await extractEvil(locator);
+  });
 });
 
-test("extract body failure when url mismatch on webkit", async () => {
-  const browser = await webkit.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.locator("body").evaluate((el, html) => {
-    el.innerHTML = html;
-  }, html);
-  await extractEvil((location) => page.locator(location).all());
+describe("extract body on happy-dom", () => {
+  let locator: (location: string) => Promise<HTMLElement[]>;
+
+  beforeAll(async () => {
+    const window = new Window();
+    const document = window.document as unknown as Document;
+    document.body.innerHTML = html;
+    document.location.href = pageUrl;
+    locator = async (location: string) =>
+      Array.from(document.querySelectorAll<HTMLElement>(location));
+  });
+
+  test("as text type", async () => {
+    await extractText(locator);
+  });
+
+  test("as html type", async () => {
+    await extractHtml(locator);
+  });
+
+  test("failure when url mismatch", async () => {
+    await extractEvil(locator);
+  });
 });
