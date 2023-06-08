a10e3b173d30b26779a2f9e83807b7c35e8603b6
MURATA, Shuuichirou <mrt@acutus.jp>
2023-06-08 11:55:52 +0900

    feat(model): Categoryモデルの説明を改善

--- packages/model/src/category.ts
+++ packages/model/src/category.ts
@@ -1,20 +1,24 @@
 import { FromSchema } from "json-schema-to-ts";
 
-// OpenRTB 2.6に基づく情報カテゴリー https://iabtechlab.com/wp-content/uploads/2022/04/OpenRTB-2-6_FINAL.pdf
+// OpenRTB 2.6の Object: Site にあるcat, cattax 類似の情報カテゴリーを持つ
+// https://iabtechlab.com/wp-content/uploads/2022/04/OpenRTB-2-6_FINAL.pdf
 const Category = {
   title: "情報カテゴリー",
   type: "object",
   properties: {
     cat: {
       title: "情報カテゴリーID",
+      description: "情報カテゴリータクソノミーcattaxで示される分類におけるコードまたはID",
       type: "string",
     },
     cattax: {
       title: "情報カテゴリータクソノミー",
+      description: "https://github.com/InteractiveAdvertisingBureau/AdCOM/blob/master/AdCOM%20v1.0%20FINAL.md#list_categorytaxonomies",
       type: "number",
     },
     name: {
       title: "情報カテゴリー名",
+      description: "情報カテゴリータクソノミーcattaxで示される分類におけるカテゴリー名",
       type: "string",
     },
   },
