649f899a27eb415f125246ee142dcaf08b69a03c
Yuki Soma <somay.work@gmail.com>
2023-06-15 15:43:35 +0900

    credential service に TSDoc を追加

--- packages/registry-service/src/credential.ts
+++ packages/registry-service/src/credential.ts
@@ -5,6 +5,17 @@ type Options = {
 };
 
 export const CredentialService = ({ prisma }: Options) => ({
+  /**
+   * 資格情報の作成
+   * @param accountId 会員 ID
+   * @param certifierId 認証機関 ID
+   * @param verifierId 検証機関 ID
+   * @param name 資格名
+   * @param issuedAt 資格発行日
+   * @param expiredAt 資格失効日
+   * @param imageUrl 資格画像の URL
+   * @return
+   */
   async create(
     accountId: string,
     certifierId: string,
