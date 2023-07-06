afa78b9effc5dd0365f6aa99804727ec2373a662
Yuki Soma <somay.work@gmail.com>
2023-07-06 11:40:27 +0900

    params.ts を共有

--- apps/registry/src/routes/admin/publisher/_id/dp/params.ts
+++ apps/registry/src/routes/admin/publisher/_id/dp/params.ts
@@ -1,13 +1,1 @@
-import { FromSchema } from "json-schema-to-ts";
-
-const Params = {
-  type: "object",
-  properties: {
-    id: { type: "string", description: "アカウントID" },
-  },
-  required: ["id"],
-} as const;
-
-type Params = FromSchema<typeof Params>;
-
-export default Params;
+export { default } from "../params";
