d90ca2d2f944c8e9332256cb96e59d8874af279d
Yuki Soma <somay.work@gmail.com>
2023-07-19 14:05:06 +0900

    generateKey() 削除

--- packages/sign/src/generate-key.ts
+++ packages/sign/src/generate-key.ts
@@ -1,30 +1,6 @@
 import { Jwk } from "@originator-profile/model";
-import { generateKeyPair, exportJWK, exportPKCS8 } from "jose";
+import { generateKeyPair, exportJWK } from "jose";
 import { createThumbprint } from "./thumbprint";
-
-/**
- * 鍵の生成
- * JWK 形式の公開鍵と PEM base64 でエンコードされた PKCS #8 プライベート鍵を生成する
- * @param alg Algorithm identifier
- */
-export async function generateKey(
-  alg = "ES256",
-): Promise<{ jwk: Jwk; pkcs8: string }> {
-  const { publicKey, privateKey } = await generateKeyPair(alg);
-  const [jwk, pkcs8] = await Promise.all([
-    exportJWK(publicKey),
-    exportPKCS8(privateKey),
-  ]);
-  if (typeof jwk.kty !== "string") throw new Error("kty is not defined");
-  return {
-    jwk: {
-      kty: jwk.kty,
-      kid: await createThumbprint(jwk),
-      ...jwk,
-    },
-    pkcs8,
-  };
-}
 
 /**
  * 鍵の生成（JWK形式）
