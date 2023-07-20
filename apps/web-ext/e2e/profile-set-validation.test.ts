94564581537838c0648b80d81684cc30a821f0d9
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-07-20 14:20:28 +0900

    ProfileSet不在時のテストを追加

---
+++ apps/web-ext/e2e/profile-set-validation.test.ts
@@ -0,0 +1,30 @@
+import { describe, beforeEach, afterEach, expect, test } from "vitest";
+import { Page } from "playwright";
+import { ctx, popup } from "./setup";
+
+describe("ProfileSet不在/不正時の確認", () => {
+  let page: Page | undefined;
+  let ext: Page | undefined;
+
+  beforeEach(async () => {
+    page = await ctx.newPage();
+    await page.goto("https://www.google.com/");
+    ext = await popup(ctx);
+  });
+
+  afterEach(async () => {
+    await Promise.all(ctx.pages().map((page) => page.close()));
+  });
+
+  test("ProfileSet不在時の確認", async () => {
+    const messages = [
+      "組織の信頼性情報と出版物の流通経路が正しく読み取れませんでした",
+      "組織の信頼性情報と出版物の流通経路がまだありません",
+      "組織の信頼性情報と出版物の流通経路の取得に失敗しました",
+    ];
+
+    for (const message of messages) {
+      expect(await ext?.locator(`:text("${message}")`).count()).toEqual(1);
+    }
+  });
+});
