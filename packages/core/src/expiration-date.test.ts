cff12370a0287abc7b6720176b1903c20e104f9b
Kohei Watanabe <nebel@fogtype.com>
2023-07-20 12:34:25 +0900

    refactor: ロケールの表記規則に従って有効期限日付時刻文字列表現に変換する処理を集約

--- packages/core/src/expiration-date.test.ts
+++ packages/core/src/expiration-date.test.ts
@@ -1,5 +1,8 @@
 import { expect, test } from "vitest";
-import { parseExpirationDate } from "./expiration-date";
+import {
+  expirationDateTimeLocaleFrom,
+  parseExpirationDate,
+} from "./expiration-date";
 
 test("ISO 8601 形式の日付", () => {
   expect(parseExpirationDate("2023-07-31").getTime()).toEqual(
@@ -17,4 +20,16 @@ test("ISO 8601 形式の日付・時刻2", () => {
   expect(
     parseExpirationDate("2023-07-31T24:00:00.000+09:00").getTime(),
   ).toEqual(new Date("2023-07-31T15:00:00.000Z").getTime());
+});
+
+test("ロケールの表記規則に従って有効期限日付時刻文字列表現に変換1", () => {
+  expect(
+    expirationDateTimeLocaleFrom(new Date("2023-07-31T24:00:00.000+09:00")),
+  ).toBe("2023/7/31 23:59:59");
+});
+
+test("ロケールの表記規則に従って有効期限日付時刻文字列表現に変換2", () => {
+  expect(expirationDateTimeLocaleFrom("2023-07-31T24:00:00.000+09:00")).toBe(
+    "2023/7/31 23:59:59",
+  );
 });
--- packages/ui/src/components/CredentialDetail.tsx
+++ packages/ui/src/components/CredentialDetail.tsx
@@ -5,6 +5,7 @@ import {
   OpCertifier,
   OpVerifier,
 } from "@originator-profile/model";
+import { expirationDateTimeLocaleFrom } from "@originator-profile/core";
 import { getVerificationType } from "../utils/credential";
 import Image from "../components/Image";
 import Table from "./Table";
@@ -62,9 +63,7 @@ function CredentialDetail({
         />
         <TableRow
           header="有効期限"
-          data={new Date(
-            new Date(credential.expiredAt).getTime() - 1,
-          ).toLocaleString()}
+          data={expirationDateTimeLocaleFrom(credential.expiredAt)}
         />
       </Table>
     </div>
