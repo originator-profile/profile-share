cf17d8a967ed52e332c7badfe5243e44dd044f2f
Kohei Watanabe <nebel@fogtype.com>
2023-07-20 12:43:10 +0900

    test: DateオブジェクトのUTCタイムゾーンオフセットでの同一性のチェック

--- packages/core/src/expiration-date.ts
+++ packages/core/src/expiration-date.ts
@@ -4,9 +4,9 @@ import { Temporal } from "@js-temporal/polyfill";
  * 有効期限の文字列表現のパース
  * @param input 入力
  * @example ISO 8601 形式の日付の場合、その日を有効期限とみなします
- * parseExpirationDate("2023-07-31") // 2023-08-01T00:00:00.000+09:00 (実行環境のタイムゾーンに従います)
+ * parseExpirationDate("2023-07-31") // 2023-07-31T15:00:00.000Z (タイムゾーンが省略された場合、実行環境のタイムゾーンに従い解釈されます)
  * @example 時刻が含まれる場合、期限切れとなる日時とみなします
- * parseExpirationDate("2023-07-31T24:00:00.000+09:00") // 2023-08-01T00:00:00.000+09:00
+ * parseExpirationDate("2023-07-31T24:00:00.000+09:00") // 2023-07-31T15:00:00.000Z
  * @return 期限切れ日時
  */
 export function parseExpirationDate(input: string): Date {
