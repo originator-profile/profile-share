c14b5ecede1ecae31762fae0f829c42c645439d2
MURATA, Shuuichirou <mrt@acutus.jp>
2023-07-24 11:56:14 +0900

    feat: globalである必要はないのでただのexported from moduleな変数に

--- apps/registry/src/prisma-client.ts
+++ apps/registry/src/prisma-client.ts
@@ -1,7 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient();
