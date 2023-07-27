7b183fbc2a2c3b09cf03de18015a4a97a4406181
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-07-27 15:15:37 +0900

    テスト内容をエンドポイントありかつ取得できない、エンドポイントなしの2つに変更、それに伴いコード修正

--- apps/web-ext/e2e/profile-set-validation.test.ts
+++ apps/web-ext/e2e/profile-set-validation.test.ts
@@ -1,10 +1,10 @@
-import { describe, beforeEach, afterEach, expect, test } from "vitest";
+import { describe, afterEach, expect, test, afterAll } from "vitest";
 import { Page } from "playwright";
 import { ctx, popup } from "./setup";
 
 describe("ProfileSet不在/不正時の確認", () => {
-  let page: Page | undefined;
   let ext: Page | undefined;
+  let page: Page | undefined;
 
   type Response = {
     status: number,
@@ -13,17 +13,53 @@ describe("ProfileSet不在/不正時の確認", () => {
   };
   
   const responseMap: Record<string, Response> = {
+    "/test": {
+      status: 200,
+      contentType: "text/html",
+      body: `
+        <!doctype html>
+        <html lang="ja">
+        <head>
+          <meta charset="utf-8">
+          <title>Title</title>
+          <link
+            href="/ps.json"
+            rel="alternate"
+            type="application/ld+json"
+          />
+        </head>
+        <body><h1>ProfileSet不在時(エンドポイントあり、取得できない)の確認</h1></body>
+        </html>
+      `,
+    },
     "/": {
       status: 200,
       contentType: "text/html",
-      body: "<!doctype html><html lang=\"ja\"><head><title>Test page</title></head><body><h1>Test page</h1></body></html>",
-    }
+      body: `
+        <!doctype html>
+        <html lang="ja">
+        <head>
+          <meta charset="utf-8">
+          <title>Title</title>
+        </head>
+        <body>
+          <h1>ProfileSet不在時(エンドポイントなし)の確認</h1>
+        </body>
+        </html>
+      `,
+    },
   };
 
-  beforeEach(async () => {
+  async function runTest(url: string): Promise<Page> {
     page = await ctx.newPage();
+
     await page.route("**", (route) => {
       const url = new URL(route.request().url());
+
+      if (url.origin === "http://localhost:8080" && url.pathname === "/test") {
+        return route.abort();
+      }
+
       const response = responseMap[url.pathname];
 
       if (response) {
@@ -33,17 +69,13 @@ describe("ProfileSet不在/不正時の確認", () => {
       }
     });
 
-    await page.goto("http://localhost:8080/");
+    try{
+      await page.goto(url);
+    }catch(err){
+      console.error(`Error navigating to ${url}`);
+    }
     ext = await popup(ctx);
-  });
 
-  afterEach(async ({ task }) => {
-    await page?.screenshot({ path: `screenshots/${task.name}-webpage.png` });
-    await ext?.screenshot({ path: `screenshots/${task.name}-web-ext.png` });
-    await Promise.all(ctx.pages().map((page) => page.close()));
-  });
-
-  test("ProfileSet不在時の確認", async () => {
     const messages = [
       "組織の信頼性情報と出版物の流通経路が正しく読み取れませんでした",
       "組織の信頼性情報と出版物の流通経路がまだありません",
@@ -57,5 +89,24 @@ describe("ProfileSet不在/不正時の確認", () => {
     counts.forEach((count) => {
       expect(count).toEqual(1);
     });
+
+    return page;
+  }
+
+  afterEach(async ({ task }) => {
+    await page?.screenshot({ path: `screenshots/${task.name}-webpage.png` });
+    await ext?.screenshot({ path: `screenshots/${task.name}-web-ext.png` });
+  });
+  
+  afterAll(async () => {
+    await Promise.all(ctx.pages().map((page) => page.close()));
+  });
+
+  test("ProfileSet不在時(エンドポイントなし)の確認", async () => {
+    page = await runTest("http://localhost:8080/");
+  });
+
+  test("ProfileSet不在時(エンドポイントあり、取得できない)の確認", async () => {
+    page = await runTest("http://localhost:8080/test");
   });
 });
