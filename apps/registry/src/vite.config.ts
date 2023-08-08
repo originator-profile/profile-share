4a7477a997a244057cbde6ea2c31a06b5a380778
Yuki Soma <somay.work@gmail.com>
2023-08-08 18:56:26 +0900

    vite.config.ts を移動

---
+++ apps/registry/src/vite.config.ts
@@ -0,0 +1,9 @@
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: join(dirname(fileURLToPath(new URL(import.meta.url))), '../frontend'),
+  plugins: [react()],
+});
