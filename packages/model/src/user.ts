8a29ec9bb109a2fde6ee36a7507bee3ebd6b16ae
Kimiaki Kuno <knokmki612@gmail.com>
2023-08-09 16:40:30 +0900

    feat: ユーザーアカウント、申請、審査のモデル

---
+++ packages/model/src/user.ts
@@ -0,0 +1,22 @@
+import { FromSchema } from "json-schema-to-ts";
+
+const User = {
+  title: "ユーザー",
+  description: "ユーザー",
+  type: "object",
+  properties: {
+    id: {
+      title: "ユーザーアカウント識別子",
+    },
+    email: {
+      title: "メールアドレス",
+      type: "string",
+    },
+  },
+  required: ["id", "email"],
+  additionalProperties: false,
+} as const;
+
+type User = FromSchema<typeof User>;
+
+export default User;
