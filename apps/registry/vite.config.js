2dbaa35465289363d42c67636f1c0df89c960261
Yuki Soma <somay.work@gmail.com>
2023-08-15 14:25:07 +0900

    apps/ui -> packages/registry-ui に変更

--- apps/registry/vite.config.js
+++ apps/registry/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: join(dirname(fileURLToPath(new URL(import.meta.url))), "../ui"),
+  root: join(
+    dirname(fileURLToPath(new URL(import.meta.url))),
+    "../../packages/registry-ui",
+  ),
   plugins: [react()],
 });
