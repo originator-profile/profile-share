83dd41062f5d66d67a5f9db06f6040bd92ac9c8c
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-08-16 17:12:03 +0900

    禁止ケースの見た目の追加

---
+++ apps/web-ext/src/components/Prohibition.tsx
@@ -0,0 +1,65 @@
+import { Dialog } from "@headlessui/react";
+import { ProjectTitle, ProjectSummary } from "@originator-profile/ui";
+import { Icon } from '@iconify/react';
+
+function WarningDetails() {
+  return (
+    <>
+      <br />
+      <summary>このメッセージが表示される理由についてもっと詳しく</summary>
+      <h2>Originator Profile ではこのサイトが本物であるかの確認を「認証鍵」という電子証明によって行っています。</h2>
+      <br />
+      <h2>このメッセージが表示されている場合、以下の2つの原因が考えられます。</h2>
+      <br />
+      <ol>
+        <li>(悪意のない正当な組織担当者が)認証鍵の取り扱いについて何らかの誤りがある</li>
+        <li>(悪意のある第三者が)認証鍵を改ざんし、偽サイトを本物のように見せようとしている</li>
+      </ol>
+      <br />
+      <h2>特に 2. の場合はあなたを欺き不利益を与える危険なサイトと考えられます。</h2>
+      <br />
+      <h2>これらの理由によりこのサイトが偽サイトである可能性をあなたにお伝えしています。</h2>
+      <h2>証明書は改ざん・偽装されている可能性があるのでご注意ください。</h2>
+      <br />
+      <h2 className="text-gray-500 ">
+        <a href="#">
+          上記を理解して組織情報や出版物の内容を閲覧する
+        </a>
+      </h2>
+    </>
+  );
+}
+
+function Prohibition() {
+  return (
+    <Dialog open onClose={() => {}}>
+      <Dialog.Panel className="fixed top-0 left-0 z-10 bg-white w-screen h-screen overflow-y-auto">
+        <main className="px-4 py-12">
+          <ProjectTitle className="mb-12" as="header" />
+          <h1 className="flex items-center flex-col gap-4 mb-12">
+            <p className="warning-text01 warning-text">
+              <Icon icon="clarity:exclamation-triangle-line" className="icon"/> アクセスにはご注意ください
+            </p>
+            <br />
+            <p className="warning-text02 warning-text">このサイトの発信元が確認できません</p>
+          </h1>
+          <article className="mb-12 max-w-sm mx-auto">
+            <h2 className="custom-text">
+              本物そっくりの偽サイトにログインしたり個人情報を登録したり支払いをしてしまい被害に合うケースが多発しています。
+              <br />
+              このページではサイトの運営者情報が確認できませんでした。そのため、この<strong className="custom-text-strong">サイトが本物かどうかは充分に注意してください。</strong>
+            </h2>
+            <br />
+            <details className="text-gray-700 ">
+              <WarningDetails />
+            </details>
+            <br />
+            <ProjectSummary as="footer" />
+          </article>
+        </main>
+      </Dialog.Panel>
+    </Dialog>
+  );
+}
+
+export default Prohibition;
