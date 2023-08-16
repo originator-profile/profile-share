970840a8db25b01a459bbc62f2e585b763198fab
Yuki Soma <somay.work@gmail.com>
2023-08-16 18:52:24 +0900

    vite.config.js 内の定数を共通化

--- apps/registry/vite.config.js
+++ apps/registry/vite.config.js
@@ -1,24 +1,26 @@
-import { join, dirname } from "node:path";
+import { join, dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import generouted from "@generouted/react-router/plugin";
 
-const REGISTRY_UI_DIRECTORY = "../../packages/registry-ui";
-
-export default defineConfig({
-  root: join(
+const REGISTRY_UI_ROOT = resolve(
+  join(
     dirname(fileURLToPath(new URL(import.meta.url))),
     "../../packages/registry-ui",
   ),
+);
+
+export default defineConfig({
+  root: REGISTRY_UI_ROOT,
   plugins: [
     react(),
     generouted({
       source: {
-        routes: `${REGISTRY_UI_DIRECTORY}/src/pages/**/[\\w[-]*.{jsx,tsx}`,
-        modals: `${REGISTRY_UI_DIRECTORY}/src/pages/**/[+]*.{jsx,tsx}`,
+        routes: `${REGISTRY_UI_ROOT}/src/pages/**/[\\w[-]*.{jsx,tsx}`,
+        modals: `${REGISTRY_UI_ROOT}/src/pages/**/[+]*.{jsx,tsx}`,
       },
-      output: `${REGISTRY_UI_DIRECTORY}/src/router.ts`,
+      output: `${REGISTRY_UI_ROOT}/src/router.ts`,
     }),
   ],
 });
