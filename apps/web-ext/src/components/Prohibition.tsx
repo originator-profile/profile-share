6d8a2d18cc326634f13781e2a0d6b94ffaad682e
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-08-22 15:04:40 +0900

    brを段落(p要素)内のみで使用するように変更、それ以外での改行はpt-3で行うように修正

--- apps/web-ext/src/components/Prohibition.tsx
+++ apps/web-ext/src/components/Prohibition.tsx
@@ -5,22 +5,20 @@ import { Icon } from '@iconify/react';
 function WarningDetails() {
   return (
     <div className="pt-3">
-      <p>Originator Profile ではこのサイトが本物であるかの確認を「認証鍵」という電子証明によって行っています。</p>
-      <br />
-      <p>このメッセージが表示されている場合、以下の2つの原因が考えられます。</p>
-      <br />
-      <ol className="px-0.5 list-decimal">
+      <p>
+        Originator Profile ではこのサイトが本物であるかの確認を「認証鍵」という電子証明によって行っています。<br />
+        このメッセージが表示されている場合、以下の2つの原因が考えられます
+      <ol className="pt-3 px-0.5 list-decimal">
         <li>(悪意のない正当な組織担当者が)認証鍵の取り扱いについて何らかの誤りがある</li>
         <li>(悪意のある第三者が)認証鍵を改ざんし、偽サイトを本物のように見せようとしている</li>
       </ol>
       <br />
-      <p>特に 2. の場合はあなたを欺き不利益を与える危険なサイトと考えられます。</p>
-      <br />
-      <p>これらの理由によりこのサイトが偽サイトである可能性をあなたにお伝えしています。</p>
-      <p>証明書は改ざん・偽装されている可能性があるのでご注意ください。</p>
-      <br />
-      <p className="text-gray-500 pb-3">
+        特に 2. の場合はあなたを欺き不利益を与える危険なサイトと考えられます。<br />
+        これらの理由によりこのサイトが偽サイトである可能性をあなたにお伝えしています。<br />
+        証明書は改ざん・偽装されている可能性があるのでご注意ください。<br />
+      <div className="text-gray-500 pb-3 pt-3">
         上記を理解して組織情報や出版物の内容を閲覧する
+      </div>
       </p>
     </div>
   );
@@ -36,25 +34,25 @@ function Prohibition() {
         <main className="px-4 py-12">
           <ProjectTitle className="mb-12" as="header" />
           <h1 className="flex items-center flex-col gap-4 mb-12">
-            <p className="text-red-700 h-7 text-base font-normal tracking-normal text-center whitespace-nowrap inline-block align-middle">
+            <div className="text-red-700 h-7 text-base font-normal tracking-normal text-center whitespace-nowrap inline-block align-middle">
               <Icon icon="clarity:exclamation-triangle-line" className="text-red-700 text-5xl font-normal tracking-normal leading-10 text-center w-11 h-11 inline-block vertical-align"/> アクセスにはご注意ください
-            </p>
+            </div>
             <br />
-            <p className="text-red-700 text-xl font-bold tracking-normal text-center w-96 h-6 inline-block align-middle">このサイトの発信元が確認できません</p>
+            <div className="text-red-700 text-xl font-bold tracking-normal text-center w-96 h-6 inline-block align-middle">
+              このサイトの発信元が確認できません
+            </div>
           </h1>
           <article className="mb-12 max-w-sm mx-auto">
             <p className="text-sm tracking-normal text-left font-normal">
-              本物そっくりの偽サイトにログインしたり個人情報を登録したり支払いをしてしまい被害に合うケースが多発しています。
-              <br />
+              本物そっくりの偽サイトにログインしたり個人情報を登録したり支払いをしてしまい被害に合うケースが多発しています。<br />
               このページではサイトの運営者情報が確認できませんでした。そのため、この<strong className="font-bold">サイトが本物かどうかは充分に注意してください。</strong>
             </p>
-            <br />
-            <details className="text-gray-700">
-              <summary>このメッセージが表示される理由についてもっと詳しく...</summary>
-                <WarningDetails />
+            <details className="text-gray-700 pt-3">
+              <WarningDetails />
             </details>
-            <br />
-            <ProjectSummary as="footer" />
+            <div className="pt-3">
+              <ProjectSummary as="footer" />
+            </div>
           </article>
         </main>
       </Dialog.Panel>
