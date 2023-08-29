52741dc436c22aef1c749c9357d2c4b58cac6cde
Shuuichirou Murata <31531654+mrt0a@users.noreply.github.com>
2023-08-29 15:13:14 +0900

    feat(ci): e2eテストでplaywright-reportを作成してartifactsにアップロードする (#865)
    * feat(ci): e2eテスト失敗時playwright-reportをアップロードする
    
    * feat(wordpress,e2e): HTMLリポートを残す
    
    * e2eテストを失敗させるため並列度を上げる
    
    * fix(ci): playwright-reportのartifactに名前をつけた
    
    * fix(ci): e2eテストの結果をひとつのartifactにまとめる
    
    * feat(web-ext,e2e): test runnerを@playwright/testに変更
    
    * fix: テストレポートを除外
    
    * feat(ci): web-extのレポートをアップロード
    
    * chore: lint and format
    
    * fix(lint): playwright-reportはlint非対象に
    
    * chore(web-ext): vitestで不要になったe2eモードを削除
    
    * fix(ci): mergeのためe2eテスト実行の並列度をもとに戻す
    
    * Update .github/actions/e2e/action.yml

--- apps/web-ext/e2e/certify.test.ts
+++ apps/web-ext/e2e/certify.test.ts
@@ -1,57 +1,50 @@
-import { describe, beforeEach, afterEach, expect, test } from "vitest";
-import { Page } from "playwright";
-import { ctx, popup } from "./setup";
+import { Page } from "@playwright/test";
+import { expect, popup, test } from "./fixtures";
 
-describe("certify", () => {
-  let page: Page | undefined;
-  let ext: Page | undefined;
+test.describe.configure({ mode: "serial" });
+let ext: Page | undefined;
 
-  beforeEach(async () => {
-    page = await ctx.newPage();
-    // Profile Registry にアクセス (apps/registry)
-    await page.goto("http://localhost:8080/");
-    ext = await popup(ctx);
-  });
+test.beforeEach(async ({ context, page }) => {
+  // Profile Registry にアクセス (apps/registry)
+  await page.goto("http://localhost:8080/");
+  ext = await popup(context);
+});
 
-  afterEach(async ({ task }) => {
-    await page?.screenshot({ path: `screenshots/${task.name}-本体.png` });
-    await ext?.screenshot({ path: `screenshots/${task.name}-拡張機能.png` });
-    await Promise.all(ctx.pages().map((page) => page.close()));
-  });
+test.afterEach(async ({ page }, testInfo) => {
+  await page.screenshot({ path: `screenshots/${testInfo.title}-本体.png` });
+  await ext?.screenshot({ path: `screenshots/${testInfo.title}-拡張機能.png` });
+});
 
-  test("拡張機能画面での認証および対象ページのマークを確認", async () => {
-    // 拡張機能ウィンドウの状態
-    expect(await ext?.title()).toMatch(/コンテンツ情報/);
-    expect(
-      await ext?.locator(':text("この記事を発行した組織") + p').textContent(),
-    ).toMatch("Originator Profile 技術研究組合");
-    expect(
-      await ext?.locator(':text("この組織は認証を受けています")').count(),
-    ).toEqual(1);
-    expect(
-      await ext
-        ?.locator('span:has-text("ブランドセーフティ認証")')
-        .textContent(),
-    ).toMatch(/自己検証/);
-    expect(
-      await ext
-        ?.locator('p:has-text("ブランドセーフティ認証") + p')
-        .textContent(),
-    ).toMatch("有効期限内");
+test("拡張機能画面での認証および対象ページのマークを確認", async ({ page }) => {
+  // 拡張機能ウィンドウの状態
+  expect(await ext?.title()).toMatch(/コンテンツ情報/);
+  expect(
+    await ext?.locator(':text("この記事を発行した組織") + p').textContent(),
+  ).toMatch("Originator Profile 技術研究組合");
+  expect(
+    await ext?.locator(':text("この組織は認証を受けています")').count(),
+  ).toEqual(1);
+  expect(
+    await ext?.locator('span:has-text("ブランドセーフティ認証")').textContent(),
+  ).toMatch(/自己検証/);
+  expect(
+    await ext
+      ?.locator('p:has-text("ブランドセーフティ認証") + p')
+      .textContent(),
+  ).toMatch("有効期限内");
 
-    // 対象のWebページにオーバーレイ表示が読み込まれるまで待機
-    await page?.waitForSelector("iframe");
+  // 対象のWebページにオーバーレイ表示が読み込まれるまで待機
+  await page.waitForSelector("iframe");
 
-    // 対象Webページにマークは表示されているか
-    expect(await page?.title()).toMatch(/OP 確認くん/);
-    expect(
-      await page
-        ?.frameLocator("iframe")
-        .getByRole("button", {
-          name: "Originator Profile 技術研究組合 OP 確認くん OP 確認くん",
-        })
-        .count(),
-      "ピンが少なくとも1つ存在する",
-    ).toBeGreaterThanOrEqual(1);
-  });
+  // 対象Webページにマークは表示されているか
+  expect(await page.title()).toMatch(/OP 確認くん/);
+  expect(
+    await page
+      .frameLocator("iframe")
+      .getByRole("button", {
+        name: "Originator Profile 技術研究組合 OP 確認くん OP 確認くん",
+      })
+      .count(),
+    "ピンが少なくとも1つ存在する",
+  ).toBeGreaterThanOrEqual(1);
 });
--- apps/web-ext/vitest.config.ts
+++ apps/web-ext/vitest.config.ts
@@ -1,19 +1,7 @@
-import { UserConfig, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const tests = {
-    test: {
-      dir: "src",
-    },
-    e2e: {
-      dir: "e2e",
-      setupFiles: "e2e/setup.ts",
-      singleThread: Boolean(process.env.CI),
-      testTimeout: 20_000,
-    },
-  };
-  const config: UserConfig = {
-    test: tests[mode as "test" | "e2e"],
-  };
-  return config;
+export default defineConfig({
+  test: {
+    dir: "src",
+  },
 });
