3e74549ff78d5f2e0b4e116480ad924d264dc129
Yuki Soma <somay.work@gmail.com>
2023-09-01 15:45:06 +0900

    JWT 確認用の API を削除

--- apps/registry/src/routes/internal/accounts/index.ts
+++ apps/registry/src/routes/internal/accounts/index.ts
@@ -3,13 +3,6 @@ import { FromHandler } from "../../../types";
 import { create, schema } from "./create";
 
 async function index(fastify: FastifyInstance): Promise<void> {
-  fastify.get(
-    "/",
-    {
-      // preValidation: fastify.authenticate
-    },
-    (r) => r.user,
-  );
   fastify.post<FromHandler<typeof create>>("/", { schema: schema }, create);
 }
 
