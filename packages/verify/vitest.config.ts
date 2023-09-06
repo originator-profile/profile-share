fb8ff13cd74e1b4f9a6a028d64853b4fe93a87d4
Shuuichirou Murata <31531654+mrt0a@users.noreply.github.com>
2023-09-06 16:38:55 +0900

    playwrightによるテストはplaywright runnerで実行する (#894)
    * test(verify): extract-body.test.tsをplaywright runnerで実行
    
    * bot: format [skip ci]
    
    * fix(verify): playwrightへの依存は不要
    
    * test(verify): happy-domを使ったtestを復活
    
    * feat(ci): yarn test時も失敗時のplaywright-reportを保存する
    
    * chore(verify): happy-dom,実ブラウザ分離によりLocaleは不要になった
    
    * chore(test): 実行に時間のかかるplaywrightによるテストをe2eに移動
    
    ---------
    
    Co-authored-by: bot <bot@originator-profile.org>

--- packages/verify/vitest.config.ts
+++ packages/verify/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    // TODO: ほんとはPlaywrightを使うような時間のかかるテストはユニットテストから切り出すのがよいかもしれないが未対応。
-    hookTimeout: 20_0000,
+    dir: "src",
   },
 });
