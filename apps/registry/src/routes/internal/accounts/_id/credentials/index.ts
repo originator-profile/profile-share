49b120ff30a71a0c79b3cf461d86894baf9897ad
Yuki Soma <somay.work@gmail.com>
2023-09-07 15:26:52 +0900

    資格情報登録 API

---
+++ apps/registry/src/routes/internal/accounts/_id/credentials/index.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from "fastify";
+import { FromHandler } from "../../../../../types";
+import { create, schema } from "./create";
+import Params from "./params";
+
+async function index(fastify: FastifyInstance): Promise<void> {
+  fastify.post<FromHandler<typeof create, Params>>(
+    "/",
+    { schema: schema },
+    create
+  );
+}
+
+export default index;
