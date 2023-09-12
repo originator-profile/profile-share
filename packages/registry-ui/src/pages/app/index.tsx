4d0030343b142a5be8bf93fc43d8f671088b7ee8
Kimiaki Kuno <knokmki612@gmail.com>
2023-09-12 10:34:25 +0900

    feat: アカウントの役割に応じた画面遷移

--- packages/registry-ui/src/pages/app/index.tsx
+++ packages/registry-ui/src/pages/app/index.tsx
@@ -1,33 +1,65 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useAsync } from "react-use";
+import useSWR from "swr";
+import { Navigate } from "react-router-dom";
+
+const useUser = (token?: string) =>
+  useSWR<{
+    id: string;
+    name: string;
+    email: string;
+    picture: string;
+    accountId: string;
+  }>(
+    token ? ["/internal/user-accounts/", token] : null,
+    async ([key, token]) => {
+      const response = await fetch(key, {
+        method: "PUT",
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      if (!response.ok) throw new Error(data);
+      return data;
+    }
+  );
+
+const useAccount = (token?: string, accountId?: string) =>
+  useSWR<{ roleValue: string }>(
+    token && accountId ? ["/internal/accounts/", accountId, token] : null,
+    async ([key, accountId, token]) => {
+      const response = await fetch(`${key + accountId}/`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      if (!response.ok) throw new Error(data);
+      return data;
+    }
+  );
 
 export default function Index() {
-  const {
-    loginWithRedirect,
-    logout,
-    isLoading,
-    isAuthenticated,
-    user,
-    getAccessTokenSilently,
-  } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
-  useAsync(async () => {
-    const token = await getAccessTokenSilently();
-    await fetch("/internal/user-accounts/", {
-      method: "PUT",
-      headers: { Authorization: `Bearer ${token}` },
-    });
+  const { value: token } = useAsync(async () => {
+    return getAccessTokenSilently();
   });
+  const { data: user } = useUser(token);
+  const { data: account } = useAccount(token, user?.accountId);
 
-  if (isLoading) {
+  if (!account) {
     return (
-      <div className="jumpu-spinner">
-        <svg viewBox="25 25 50 50">
-          <circle cx="50" cy="50" r="20" />
-        </svg>
-      </div>
+      <>
+        <div className="jumpu-spinner">
+          <svg viewBox="25 25 50 50">
+            <circle cx="50" cy="50" r="20" />
+          </svg>
+        </div>
+      </>
     );
   }
 
-  // TODO: 適切なページに画面遷移して
+  if (account.roleValue === "group") return <Navigate to="./request-op" />;
+  if (account.roleValue === "certifier") return <Navigate to="./review-op" />;
+
+  // TODO: 画面遷移できなかった場合の見た目を実装して
+  return <p>なにかがうまくいきませんでした</p>;
 }
