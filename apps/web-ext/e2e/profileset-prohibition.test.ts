b38ba1a0960f003bdb14048f86d8f3a14f5e55ae
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-09-14 11:07:27 +0900

    テスト名の変更、OP検証失敗時のケース追加、鍵ファイルのクリーンナップ処理追加

--- apps/web-ext/e2e/profileset-prohibition.test.ts
+++ apps/web-ext/e2e/profileset-prohibition.test.ts
@@ -1,6 +1,6 @@
 import { Page } from "@playwright/test";
 import {expect, popup, test } from "./fixtures";
-import { execSync } from 'child_process'; 
+import { execSync } from 'node:child_process'; 
 
 function executeCommand(command: string, directory?: string): void {
   try {
@@ -17,8 +17,6 @@ function executeCommand(command: string, directory?: string): void {
 
 test.describe.configure({ mode: "serial" });
 
-
-
 let ext: Page | undefined;
 
 test.afterEach(async ({ page }, testInfo) => {
@@ -26,10 +24,12 @@ test.afterEach(async ({ page }, testInfo) => {
   if (ext && !ext.isClosed()) {
     await ext.screenshot({ path: `screenshots/${testInfo.title}-web-ext.png` });
   }
+  executeCommand('rm evil.priv.json', '../registry');
+  executeCommand('rm evil.pub.json', '../registry');
   executeCommand('bin/dev publisher:website -i account-key.example.priv.json --id localhost --input website.example.json -o update', '../registry');
 });
 
-test("ProfileSet禁止時の確認", async ({ context, page }) => {
+test("DPの検証失敗時は閲覧を禁止する", async ({ context, page }) => {
   executeCommand('bin/dev key-gen --output evil', '../registry');
   executeCommand('bin/dev publisher:website -i evil.priv.json --id localhost --input website.example.json -o update', '../registry');
   
@@ -43,8 +43,28 @@ test("ProfileSet禁止時の確認", async ({ context, page }) => {
     "本物そっくりの偽サイトにログインしたり個人情報を登録したり支払いをしてしまい被害に合うケースが多発しています。このページではサイトの運営者情報が確認できませんでした。そのため、このサイトが本物かどうかは充分に注意してください。",
   ];
 
-  for (const text of warningTexts) {
-    const foundText = await ext.textContent(`:text("${text}")`);
+  await Promise.all(warningTexts.map(async (text) => {
+    const foundText = await ext?.textContent(`:text("${text}")`);
     expect(foundText).toBe(text);
-  }
+  }));
+  });
+
+test("OPの検証失敗時は閲覧を禁止する", async ({ context, page }) => {
+  executeCommand('bin/dev key-gen --output evil', '../registry');
+  executeCommand('bin/dev cert:issue -i evil.priv.json --certifier localhost --holder localhost', '../registry');
+  
+  await page.goto("http://localhost:8080/");
+  
+  ext = await popup(context);
+
+  const warningTexts = [
+    " アクセスにはご注意ください",
+    "このサイトの発信元が確認できません",
+    "本物そっくりの偽サイトにログインしたり個人情報を登録したり支払いをしてしまい被害に合うケースが多発しています。このページではサイトの運営者情報が確認できませんでした。そのため、このサイトが本物かどうかは充分に注意してください。",
+  ];
+
+  await Promise.all(warningTexts.map(async (text) => {
+    const foundText = await ext?.textContent(`:text("${text}")`);
+    expect(foundText).toBe(text);
+  }));
   });
