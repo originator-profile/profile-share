74ab1b119ce43b0e128f06486f527595234eedc9
Yuki Soma <somay.work@gmail.com>
2023-09-20 19:45:32 +0900

    credentials/id のスキーマを修正

--- apps/registry/src/routes/internal/accounts/_id/credentials/_credentialId/update.ts
+++ apps/registry/src/routes/internal/accounts/_id/credentials/_credentialId/update.ts
@@ -21,7 +21,7 @@ const schema: FastifySchema = {
   body: Body,
   params: Params,
   description: "資格情報の更新",
-  security: [],
+  security: [{ bearerAuth: ["write:requests"] }],
   response: {
     200: {
       title: "会員",
