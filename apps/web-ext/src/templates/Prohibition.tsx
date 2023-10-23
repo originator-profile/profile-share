c29e0e9bde67a33172c4e00b1a138a7aadad4353
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-10-23 17:18:42 +0900

    WarningDetailsをProhibitionのスコープ外で定義

--- apps/web-ext/src/templates/Prohibition.tsx
+++ apps/web-ext/src/templates/Prohibition.tsx
@@ -9,50 +9,50 @@ type ProhibitionProps = {
   tabId: number;
 };
 
-  function Prohibition({ dp, tabId }: ProhibitionProps) {
-  function WarningDetails() {
-    return (
-      <div className="pt-3">
-        <p>
-          Originator Profile
-          ではこのサイトが本物であるかの確認を「認証鍵」という電子証明によって行っています。
-          <br />
-          このメッセージが表示されている場合、以下の2つの原因が考えられます
-        </p>
-        <ol className="pt-3 px-0.5 list-decimal">
-          <li>
-            (悪意のない正当な組織担当者が)認証鍵の取り扱いについて何らかの誤りがある
-          </li>
-          <li>
-            (悪意のある第三者が)認証鍵を改ざんし、偽サイトを本物のように見せようとしている
-          </li>
-        </ol>
-        <p className="pt-3">
-          特に 2. の場合はあなたを欺き不利益を与える危険なサイトと考えられます。
-          <br />
-          これらの理由によりこのサイトが偽サイトである可能性をあなたにお伝えしています。
-          <br />
-          証明書は改ざん・偽装されている可能性があるのでご注意ください。
-          <br />
-        </p>
-
-        <div className="text-gray-500 pb-3 pt-3 cursor-pointer">
-          <Link
-            to={[
-              routes.base.build({ tabId: String(tabId) }),
-              routes.publ.build(dp),
-              "?unsafe",
-            ].join("/")}
-            className="text-gray-500"
-          >
-            上記を理解して組織情報や出版物の内容を閲覧する
-          </Link>
-        </div>
-      </div>
-    );
-  }
-
+function WarningDetails({ dp, tabId }: ProhibitionProps) {
   return (
+    <div className="pt-3">
+      <p>
+        Originator Profile
+        ではこのサイトが本物であるかの確認を「認証鍵」という電子証明によって行っています。
+        <br />
+        このメッセージが表示されている場合、以下の2つの原因が考えられます
+      </p>
+      <ol className="pt-3 px-0.5 list-decimal">
+        <li>
+          (悪意のない正当な組織担当者が)認証鍵の取り扱いについて何らかの誤りがある
+        </li>
+        <li>
+          (悪意のある第三者が)認証鍵を改ざんし、偽サイトを本物のように見せようとしている
+        </li>
+      </ol>
+      <p className="pt-3">
+        特に 2. の場合はあなたを欺き不利益を与える危険なサイトと考えられます。
+        <br />
+        これらの理由によりこのサイトが偽サイトである可能性をあなたにお伝えしています。
+        <br />
+        証明書は改ざん・偽装されている可能性があるのでご注意ください。
+        <br />
+      </p>
+
+      <div className="text-gray-500 pb-3 pt-3 cursor-pointer">
+        <Link
+          to={[
+            routes.base.build({ tabId: String(tabId) }),
+            routes.publ.build(dp),
+            "?unsafe",
+          ].join("/")}
+          className="text-gray-500"
+        >
+          上記を理解して組織情報や出版物の内容を閲覧する
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function Prohibition({ dp, tabId }: ProhibitionProps) {
+    return (
     <main className="fixed top-0 left-0 z-10 bg-white w-screen h-screen overflow-y-auto px-4 py-12">
       <ProjectTitle className="mb-12" as="header" />
       <h1 className="flex items-center flex-col gap-4 mb-12">
@@ -81,7 +81,7 @@ type ProhibitionProps = {
           <summary>
             このメッセージが表示される理由についてもっと詳しく...
           </summary>
-          <WarningDetails />
+          <WarningDetails dp={dp} tabId={tabId}/>
         </details>
         <div className="pt-3">
           <ProjectSummary as="footer" />
