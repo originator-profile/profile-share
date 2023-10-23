704ff91b8ff204f464285cf873deebbb26cd8afe
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-10-23 11:55:08 +0900

    別関数化したものをほかでも使用するためカスタムフック化、pages/Publ、pages/Orgに適用

---
+++ apps/web-ext/src/utils/usecheck-errors-and-navigate.ts
@@ -0,0 +1,40 @@
+import { findProfileErrors } from "@originator-profile/ui/src/utils";
+import { routes } from "./routes";
+import {Profile, ProfileError } from "@originator-profile/ui/src/types";
+
+type CheckResult = 
+  | { type: 'navigate'; path: string }
+  | { type: 'error'; error: ProfileError };
+
+function usecheckErrorsAndNavigate({
+  profiles, 
+  tabId,
+  queryParams
+}: {
+  profiles: Profile[];
+  tabId: number;
+  queryParams: URLSearchParams;
+}): CheckResult | undefined {
+  
+  const hasUnsafeParam = queryParams.has("unsafe");
+  const errors = findProfileErrors(profiles);
+  const hasProfileTokenVerifyFailed = errors.some(
+    (result) => result.code === "ERR_PROFILE_TOKEN_VERIFY_FAILED"
+  );
+
+  if (hasProfileTokenVerifyFailed && !hasUnsafeParam) {
+    const path = [
+      routes.base.build({ tabId: String(tabId) }),
+      routes.prohibition.build({})
+    ].join("/");
+    return { type: 'navigate', path };
+  }
+  
+  if (!hasProfileTokenVerifyFailed && errors[0]) {
+    return { type: 'error', error: errors[0] };
+  }
+
+  return;
+}
+
+export default usecheckErrorsAndNavigate;
