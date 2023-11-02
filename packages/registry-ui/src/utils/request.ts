f9fe3055edcfd66ca30bb48f676d53a3de0388b6
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-02 19:19:47 +0900

    feat: 申請取得処理でのDBモデルからデータモデルへの変換
    型を得る都合上 @prisma/client に依存するが設計上望ましくない
    将来的には @originator-profile/registry-db に移管して依存しない予定

--- packages/registry-ui/src/utils/request.ts
+++ packages/registry-ui/src/utils/request.ts
@@ -1,8 +1,35 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import useSWR from "swr";
+import useSWR, { SWRResponse } from "swr";
 import { useAsync } from "react-use";
 import fetcher from "./fetcher";
 import { Request } from "@originator-profile/model";
+import { Prisma } from "@prisma/client";
+
+type Body = Omit<
+  Prisma.requestsGetPayload<
+    Prisma.requestsDefaultArgs & { include: { reviewComments: true } }
+  >,
+  "createdAt" | "updatedAt"
+> & {
+  createdAt: string;
+  updatedAt: string;
+};
+
+type RequestSWRResponse = SWRResponse<Body>;
+
+/*
+ * DBモデルからデータモデルへの変換
+ * TODO: @originator-profile/registry-db に移して
+ */
+function convert(body: RequestSWRResponse["data"]): Request | undefined {
+  if (!body) return body;
+  return {
+    ...body,
+    requestSummary: body.requestSummary ?? undefined,
+    reviewSummary: body.reviewSummary ?? undefined,
+    status: body.statusValue as unknown as Request["status"],
+  };
+}
 
 /**
  * 最新の申請情報の取得
@@ -13,12 +40,17 @@ export function useLatestRequest(accountId: string | null) {
     return getAccessTokenSilently();
   });
 
-  return useSWR(
+  const swr: RequestSWRResponse = useSWR(
     token &&
       accountId && {
         url: `/internal/accounts/${accountId}/requests/latest/`,
         token,
       },
-    fetcher<Request>,
+    fetcher<Body>,
   );
+
+  return {
+    ...swr,
+    data: convert(swr.data),
+  };
 }
