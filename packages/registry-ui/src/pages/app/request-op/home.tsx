65bde216c6f40545a7af9f558751e1c1333b5b07
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-04 16:41:08 +0900

    feat: 申請ステータスの見た目

--- packages/registry-ui/src/pages/app/request-op/home.tsx
+++ packages/registry-ui/src/pages/app/request-op/home.tsx
@@ -1,16 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useUser } from "../../../utils/user";
 import { useLatestRequest } from "../../../utils/request";
+import RequestStatus from "../../../components/RequestStatus";
 
 function Home() {
   const { user: token } = useAuth0();
   const { data: user } = useUser(token?.sub ?? null);
   const { data: request } = useLatestRequest(user?.accountId ?? null);
-  return (
-    <article>
-      <pre>{JSON.stringify(request, null, 2)}</pre>
-    </article>
-  );
+  return <article>{request && <RequestStatus request={request} />}</article>;
 }
 
 export default Home;
