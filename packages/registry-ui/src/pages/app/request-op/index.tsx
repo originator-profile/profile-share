d60c5eaf3031727fd375e2ed9ec71057ccf126e1
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-05 03:44:44 +0900

    feat: 初回の申請時のホーム画面

--- packages/registry-ui/src/pages/app/request-op/index.tsx
+++ packages/registry-ui/src/pages/app/request-op/index.tsx
@@ -1,12 +1,49 @@
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useUser } from "../../../utils/user";
 import { useLatestRequest } from "../../../utils/request";
 import RequestStatus from "../../../components/RequestStatus";
+import logomarkTransparentUrl from "@originator-profile/ui/src/assets/logomark-transparent.png";
+
+function FirstRequest() {
+  return (
+    <article className="flex items-center gap-4">
+      <img
+        width={452}
+        height={388}
+        className="flex-1 min-w-[24rem]"
+        src={logomarkTransparentUrl}
+        alt=""
+      />
+      <section className="shrink-0">
+        <h2 className="text-2xl font-bold mb-4">
+          あらかじめご用意いただくとスムーズです
+        </h2>
+        <ul className="list-disc pl-8 text-gray-600 text-sm mb-6">
+          <li>組織の説明文</li>
+          <li>Webサイトのプライバシーポリシーや編集ガイドラインURL</li>
+          <li>企業ロゴマークの画像</li>
+          <li>事務局からの連絡用メールアドレス</li>
+          <li>取得済みの第三者認証情報</li>
+        </ul>
+        <Link
+          className="inline-flex items-center gap-2 jumpu-outlined-button font-bold"
+          to="./holder/"
+        >
+          登録をはじめる
+          <Icon icon="fa6-solid:arrow-right" />
+        </Link>
+      </section>
+    </article>
+  );
+}
 
 function Index() {
   const { user: token } = useAuth0();
   const { data: user } = useUser(token?.sub ?? null);
-  const { data: request } = useLatestRequest(user?.accountId ?? null);
+  const { data: request, error } = useLatestRequest(user?.accountId ?? null);
+  if (error?.cause?.status === 404) return <FirstRequest />;
   return <article>{request && <RequestStatus request={request} />}</article>;
 }
 
