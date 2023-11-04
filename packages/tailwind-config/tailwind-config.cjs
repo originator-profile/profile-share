3dea88325f5231bf3d4ee7957679ad9db379e1e6
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-04 16:45:00 +0900

    chore(tailwindcss)!: 安全色の追加とプライマリー色の更新
    Figmaに定義されているカラースキームに追従する目的です。
    変更前のプライマリー色を使用している箇所に影響があります。
    see https://www.figma.com/file/Cf0aPsumBfWhAeh9mLQXAe/Originator-Profile?type=design&node-id=4164-14069&mode=design&t=A0oTbzFiq4DCRsBw-4

--- packages/tailwind-config/tailwind-config.cjs
+++ packages/tailwind-config/tailwind-config.cjs
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f8fdfd",
+          100: "#f1fbfb",
+          200: "#e6f8f8",
+          300: "#cceff0",
+          400: "#99dfe1",
+          500: "#66cfd2",
+          600: "#33bfc3",
+          700: "#00afb4",
+          800: "#008387",
+          900: "#00585a",
+          DEFAULT: "#000afb4",
+        },
+        success: {
+          extralight: "#f7fee7",
+          light: "#84cc16",
+          DEFAULT: "#65a30d",
+        },
+        caution: {
+          extralight: "#f9fafb",
+          light: "#f9fafb",
+          DEFAULT: "#f3f4f6",
+        },
+        danger: {
+          extralight: "#fef2f2",
+          light: "#f87171",
+          DEFAULT: "#dc2626",
+        },
+      },
+    },
   },
   plugins: [
     ...require("@jumpu-ui/tailwindcss"),
