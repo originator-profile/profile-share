3c7aa386c6991dffa1132238df9a36c0738fc388
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-11-10 16:49:33 +0900

    既存のトップページ表示用の実装をdebuggerに移動、未ログインでAuth0のログインページ、ログイン済みでOP登録サイトが表示されるように変更

---
+++ packages/registry-ui/src/pages/debugger.tsx
@@ -0,0 +1,176 @@
+import { type ChangeEvent, type FormEvent, useState } from "react";
+import clsx from "clsx";
+import {
+  RemoteKeys,
+  ProfilesVerifier,
+  expandProfileSet,
+} from "@originator-profile/verify";
+import { ProjectSummary } from "@originator-profile/ui";
+import FormRow from "../components/FormRow";
+import AppBar from "../components/AppBar";
+
+type InitialValues = {
+  registry: string;
+  endpoint: string;
+  profileSet?: unknown;
+};
+
+function saveInitialValues(val: InitialValues) {
+  window.history.replaceState(null, "", `#${window.btoa(JSON.stringify(val))}`);
+}
+
+function loadInitialValues() {
+  try {
+    return JSON.parse(window.atob(document.location.hash.slice(1)));
+  } catch {
+    return {
+      registry: document.location.hostname,
+      endpoint: `${document.location.origin}/ps.json`,
+    };
+  }
+}
+
+const initialValues = loadInitialValues();
+
+export default function Debugger() {
+  const [values, setValues] = useState<Record<string, unknown>>({});
+  const [presentation, setPresentation] = useState(
+    "profileSet" in initialValues ? "direct" : "url",
+  );
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setPresentation(event.target.value);
+
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const registry = String(formData.get("registry"));
+    const endpoint = String(formData.get("endpoint"));
+    const jsonld = String(formData.get("jsonld"));
+
+    let profileSet;
+
+    switch (presentation) {
+      case "direct":
+        try {
+          profileSet = JSON.parse(jsonld);
+        } catch {
+          profileSet = jsonld;
+        }
+        setValues({ registry, profileSet });
+        saveInitialValues({ registry, endpoint, profileSet });
+        break;
+      case "url":
+        {
+          setValues({ registry, endpoint });
+          saveInitialValues({ registry, endpoint });
+
+          const response = await fetch(endpoint)
+            .then((res) =>
+              res.ok
+                ? res.json()
+                : new Error(`${res.status} ${res.statusText}`),
+            )
+            .catch((e: Error) => e);
+          setValues((values) => ({ ...values, response }));
+          if (response instanceof Error) return;
+
+          profileSet = response;
+        }
+        break;
+    }
+
+    const expanded = await expandProfileSet(profileSet).catch((e) => e);
+    setValues((values) => ({ ...values, expanded }));
+    if (expanded instanceof Error) return;
+
+    const jwksEndpoint = new URL(
+      import.meta.env.DEV && registry === "localhost"
+        ? `http://localhost:8080/.well-known/jwks.json`
+        : `https://${registry}/.well-known/jwks.json`,
+    );
+    const results = await ProfilesVerifier(
+      expanded,
+      RemoteKeys(jwksEndpoint),
+      registry,
+      null,
+    )();
+    setValues((values) => ({ ...values, results }));
+  }
+  return (
+    <>
+      <AppBar />
+      <article className="max-w-3xl px-4 pt-12 pb-8 mx-auto">
+        <h1 className="text-4xl font-bold mb-8">{document.title}</h1>
+        <form className="mb-8" onSubmit={onSubmit}>
+          <FormRow className="mb-4" label="Registry">
+            <input
+              className="jumpu-input flex-1"
+              name="registry"
+              required
+              defaultValue={initialValues.registry}
+            />
+          </FormRow>
+          <FormRow as="span" className="mb-4" label="Profile Set Presentation">
+            <label className="flex gap-1 items-center py-1">
+              <input
+                name="presentation"
+                type="radio"
+                value="url"
+                checked={presentation === "url"}
+                onChange={handleChange}
+              />
+              URL
+            </label>
+            <label className="flex gap-1 items-center py-1">
+              <input
+                name="presentation"
+                type="radio"
+                value="direct"
+                checked={presentation === "direct"}
+                onChange={handleChange}
+              />
+              Direct Input
+            </label>
+          </FormRow>
+          <FormRow
+            className={clsx("mb-4", { hidden: presentation !== "url" })}
+            label="Endpoint"
+          >
+            <input
+              className="jumpu-input flex-1"
+              name="endpoint"
+              hidden={presentation !== "url"}
+              type="url"
+              defaultValue={initialValues.endpoint}
+            />
+          </FormRow>
+          <FormRow
+            className={clsx("mb-4", { hidden: presentation !== "direct" })}
+            label="Profile Set"
+          >
+            <textarea
+              className="jumpu-textarea resize flex-1"
+              name="jsonld"
+              hidden={presentation !== "direct"}
+              cols={12}
+              rows={18}
+              style={{ fontFamily: "monospace" }}
+              defaultValue={
+                initialValues.profileSet
+                  ? JSON.stringify(initialValues.profileSet)
+                  : ""
+              }
+            />
+          </FormRow>
+          <input className="jumpu-button" type="submit" value="Verify" />
+        </form>
+        {Object.entries(values).length > 0 && (
+          <h2 className="text-2xl font-bold mb-4">Result</h2>
+        )}
+
+        <ProjectSummary />
+      </article>
+    </>
+  );
+}
