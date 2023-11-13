fe9efa7714b8a11a293ab313941e9f8d1b304ca2
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-13 21:01:55 +0900

    feat: 「ホーム」「お知らせ」「登録」タブの実装

--- packages/registry-ui/src/pages/app/request-op/_layout.tsx
+++ packages/registry-ui/src/pages/app/request-op/_layout.tsx
@@ -1,68 +1,91 @@
-import { Outlet, useLocation, Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import { Outlet, Link } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
+import useTabs from "../../../utils/use-tabs";
 
-type Props = {
-  children: React.ReactNode;
-};
+/**
+ * NOTE: Jumpu UIがナビゲーションタブの実装に対応しておらずARIA APGタブパターンに逸脱した実装になっている
+ * see https://github.com/tuqulore/jumpu-ui/issues/544
+ */
 
-function Layout({ children }: Props) {
-  const location = useLocation();
-  const registerTabs = [
-    {
-      route: "holder",
-      label: "組織情報",
-      status: null,
-    },
-    {
-      route: "public-key",
-      label: "公開鍵",
-      status: null,
-    },
-    {
-      route: "logo",
-      label: "ロゴマーク",
-      status: null,
-    },
-    {
-      route: "credential",
-      label: "資格情報",
-      status: null,
-    },
-  ] as const;
+function Tabs() {
+  const { registerTabsSelected, isTabSelected } = useTabs();
+  return (
+    <header className="bg-gray-50 border-b">
+      <div className="jumpu-tabs max-w-5xl px-4 mx-auto mb-[-1px]">
+        <nav role="tablist">
+          <Link
+            role="tab"
+            aria-selected={isTabSelected("request-op")}
+            className="gap-2"
+            to="./"
+          >
+            <Icon icon="iconamoon:home-light" />
+            ホーム
+          </Link>
+          <Link
+            role="tab"
+            aria-selected={isTabSelected("notifications")}
+            className="gap-2"
+            to="./notifications/"
+          >
+            <Icon icon="bx:envelope" />
+            お知らせ
+          </Link>
+          <Link
+            role="tab"
+            aria-selected={registerTabsSelected}
+            className="gap-2"
+            to="./holder/"
+          >
+            <Icon icon="fa6-solid:list-check" />
+            登録
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
 
-  // URLのパスを見て、どのタブが選択されているかを判定する
-  const isTabSelected = (pathSubstring: string) =>
-    location.pathname.split("/").filter(Boolean).pop() === pathSubstring;
-  const registerTabsSelected = registerTabs
-    .map((tab) => isTabSelected(tab.route))
-    .some((selected) => selected);
+function RegisterTabs() {
+  const { registerTabs, registerTabsSelected, isTabSelected } = useTabs();
+
+  if (!registerTabsSelected) return null;
 
   return (
+    <div className="jumpu-boxed-tabs mb-6">
+      <h1 className="text-4xl font-bold border-b min-w-[12rem]">登録</h1>
+      <nav role="tablist" className="!justify-end">
+        {registerTabs.map((registerTab) => (
+          <Link
+            role="tab"
+            aria-selected={isTabSelected(registerTab.route)}
+            className="min-w-[10rem]"
+            to={`./${registerTab.route}/`}
+          >
+            {registerTab.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+}
+
+function Article() {
+  return (
     <article className="max-w-5xl px-4 pt-12 pb-8 mx-auto">
-      {registerTabsSelected && (
-        <div className="jumpu-boxed-tabs flex justify-between mb-6">
-          <h1 className="text-4xl font-bold border-b">登録</h1>
-          {/**
-           * NOTE: .jumpu-boxed-tabs がナビゲーションタブの実装に対応しておらずARIA APGタブパターンに逸脱した実装になっている
-           * see https://github.com/tuqulore/jumpu-ui/issues/544
-           */}
-          <nav role="tablist" className="!justify-end">
-            {registerTabs.map((tab) => (
-              <Link
-                role="tab"
-                aria-selected={isTabSelected(tab.route)}
-                className="min-w-[10rem]"
-                to={tab.route}
-              >
-                {tab.label}
-              </Link>
-            ))}
-          </nav>
-        </div>
-      )}
+      <RegisterTabs />
       <Outlet />
-      {children}
     </article>
+  );
+}
+
+function Layout() {
+  return (
+    <>
+      <Tabs />
+      <Article />
+    </>
   );
 }
 
