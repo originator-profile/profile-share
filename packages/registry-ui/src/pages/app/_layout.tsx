50a529d27d47273c92a2ad16d59eee7d03b0157d
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-11-20 17:45:53 +0900

    app/debuggerへのアクセス時のみ認証不要にする

--- packages/registry-ui/src/pages/app/_layout.tsx
+++ packages/registry-ui/src/pages/app/_layout.tsx
@@ -1,19 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import AppBar from "../../components/AppBar";
+import React from "react"; // Reactをインポート
 
-type Props = {
-  children: React.ReactNode;
+type LayoutProps = {
+  children?: React.ReactNode; // ここでReact.ReactNode型を指定
 };
 
-function Layout({ children }: Props) {
-  return (
-    <>
-      <AppBar />
-      <Outlet />
-      {children}
-    </>
-  );
+const LayoutComponent: React.FC<LayoutProps> = ({ children }) => (
+  <>
+    <AppBar />
+    <Outlet />
+    {children}
+  </>
+);
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const location = useLocation();
+
+  // debuggerページの場合、認証不要のLayoutをそのまま使用
+  if (location.pathname === "/app/debugger") {
+    return <LayoutComponent>{children}</LayoutComponent>;
+  }
+
+  // それ以外のページの場合、認証が必要なLayoutを使用
+  const LayoutWithAuth = withAuthenticationRequired(LayoutComponent);
+  return <LayoutWithAuth>{children}</LayoutWithAuth>;
 }
 
-export default withAuthenticationRequired(Layout);
+export default Layout;
