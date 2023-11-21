eb46e14339d6335da56414caebddc44b6703e9fe
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-21 21:55:41 +0900

    feat: 組織のロゴ、名前、OP IDの表示

--- packages/registry-ui/src/pages/app/request-op/_layout.tsx
+++ packages/registry-ui/src/pages/app/request-op/_layout.tsx
@@ -1,7 +1,40 @@
 import { Icon } from "@iconify/react";
 import { Outlet, Link } from "react-router-dom";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import useTabs from "../../../utils/use-tabs";
+import { useUser } from "../../../utils/user";
+import { useAccount, useAccountLogo } from "../../../utils/account";
+
+function AccountHeader() {
+  const { user: token } = useAuth0();
+  const { data: user } = useUser(token?.sub ?? null);
+  const { data: account } = useAccount(user?.accountId ?? null);
+  const { data: logo } = useAccountLogo(user?.accountId ?? null);
+
+  return (
+    <header className="bg-gray-50">
+      <div className="max-w-5xl px-4 pt-4 pb-2 mx-auto flex gap-4 items-center">
+        <img
+          className="rounded border border-gray-200 bg-gray-100"
+          src={logo?.url}
+          width={52}
+          height={52}
+        />
+        {account ? (
+          <div className="flex flex-col justify-center">
+            <h2 className="font-bold text-xl">{account.name}</h2>
+            <p className="text-gray-700 text-xs">{account.domainName}</p>
+          </div>
+        ) : (
+          <div className="animate-pulse w-full flex flex-col gap-1.5 justify-center">
+            <div className="bg-slate-200 rounded min-w-[8rem] w-1/5 h-5" />
+            <div className="bg-slate-200 rounded min-w-[12rem] w-1/3 h-3" />
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
 
 /**
  * NOTE: Jumpu UIがナビゲーションタブの実装に対応しておらずARIA APGタブパターンに逸脱した実装になっている
@@ -84,6 +117,7 @@ function Article() {
 function Layout() {
   return (
     <>
+      <AccountHeader />
       <Tabs />
       <Article />
     </>
