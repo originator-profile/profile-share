507861777c83f1626a8941b3e8a17e7af9b8c7dc
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-11-22 12:28:17 +0900

    withAuthenticationRequiredによる保護は不要なので削除

--- packages/registry-ui/src/pages/app/_layout.tsx
+++ packages/registry-ui/src/pages/app/_layout.tsx
@@ -1,28 +1,12 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { Outlet } from "react-router-dom";
 import AppBar from "../../components/AppBar";
 import React from "react";
 
-type LayoutProps = {
-  children?: React.ReactNode;
-};
-
-const LayoutComponent: React.FC<LayoutProps> = ({ children }) => (
+const Layout: React.FC = () => (
   <>
     <AppBar />
     <Outlet />
   </>
 );
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const location = useLocation();
-
-  if (location.pathname === "/app/debugger") {
-    return <LayoutComponent>{children}</LayoutComponent>;
-  }
-
-  const LayoutWithAuth = withAuthenticationRequired(LayoutComponent);
-  return <LayoutWithAuth>{children}</LayoutWithAuth>;
-};
 
 export default Layout;
