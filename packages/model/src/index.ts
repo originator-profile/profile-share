ac932537ad922ee2271139f969a8227e462cfb11
Yuki Soma <somay@users.noreply.github.com>
2023-11-22 09:58:26 +0900

    packages/model に Advertisement 型を定義 (#1007)
    * model/ に advertisement 型を定義
    
    * プロパティを最小限に削減
    
    * 不要な import 削除
    
    * 最小限ということでさらにプロパティ削減

--- packages/model/src/index.ts
+++ packages/model/src/index.ts
@@ -18,3 +18,4 @@ export { default as JwtOpPayload } from "./jwt-op-payload";
 export { default as JwtDpPayload } from "./jwt-dp-payload";
 export { default as User } from "./user";
 export { default as Request } from "./request";
+export { default as Advertisement } from "./advertisement";
