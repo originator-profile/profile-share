1bb12beb3685abeedb8ffe2510367fc7812a4109
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-24 18:20:19 +0900

    test(verify): OP Token 検証時 issuer が一致しないケース

--- packages/verify/src/verify-token.test.ts
+++ packages/verify/src/verify-token.test.ts
@@ -2,6 +2,7 @@ import { test, expect } from "vitest";
 import { addYears, getUnixTime, fromUnixTime } from "date-fns";
 import { Op, Dp } from "@originator-profile/model";
 import { signOp, signDp, generateKey } from "@originator-profile/sign";
+import { ProfileTokenVerifyFailed } from "./errors";
 import { TokenDecoder } from "./decode";
 import { LocalKeys } from "./keys";
 import { TokenVerifier } from "./verify-token";
@@ -25,6 +26,26 @@ test("verify OP Token", async () => {
   const result = await verifier(jwt);
   // @ts-expect-error assert
   expect(result.op).toEqual(op);
+});
+
+test("OP Token の issuer が検証者にとって未知ならば検証に失敗", async () => {
+  const iat = getUnixTime(new Date());
+  const exp = getUnixTime(addYears(new Date(), 10));
+  const op: Op = {
+    type: "op",
+    issuedAt: fromUnixTime(iat).toISOString(),
+    expiredAt: fromUnixTime(exp).toISOString(),
+    issuer: "evil.example.org",
+    subject: "example.com",
+    item: [],
+  };
+  const { publicKey, privateKey } = await generateKey();
+  const decoder = TokenDecoder(null);
+  const keys = LocalKeys({ keys: [publicKey] });
+  const verifier = TokenVerifier(keys, "example.org", decoder);
+  const jwt = await signOp(op, privateKey);
+  const result = await verifier(jwt);
+  expect(result).toBeInstanceOf(ProfileTokenVerifyFailed);
 });
 
 test("verify DP Token", async () => {
