8f640ea0b7ddddf63f76b948ff9c5b0d729db631
Yuki Soma <somay@users.noreply.github.com>
2023-11-24 19:00:38 +0900

    テストコンテキスト名修正
    Co-authored-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/verify/src/fetch-website-profile-pair.test.ts
+++ packages/verify/src/fetch-website-profile-pair.test.ts
@@ -6,7 +6,7 @@ import { JsonLdDocument } from "jsonld";
 import { ProfilesFetchFailed } from "./errors";
 import { fetchWebsiteProfilePair } from "./fetch-website-profile-pair";
 
-describe("fetch-profiles", async () => {
+describe("fetch-website-profile-pair", async () => {
   const profiles: JsonLdDocument = {
     "@context": "https://originator-profile.org/context.jsonld",
     website: {
