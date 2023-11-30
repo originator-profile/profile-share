279fc74258203c9c3c8c4d095e2051ad12e936f5
Kimiaki Kuno <knokmki612@gmail.com>
2023-11-30 23:47:03 +0900

    refactor: サニタイズ処理をカスタムフックとして外部化

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -4,3 +4,4 @@ export * from "./role";
 export { default as sortDps } from "./sort-dps";
 export { default as toProfile } from "./to-profile";
 export { default as useModal } from "./use-modal";
+export { default as useSanitizedHtml } from "./use-sanitized-html";
---
+++ packages/ui/src/utils/use-sanitized-html.ts
@@ -0,0 +1,18 @@
+import DOMPurify from "dompurify";
+
+/** HTML文字列をサニタイズするカスタムフック */
+export default function useSanitizedHtml(
+  dangerousHtml?: string,
+): string | undefined {
+  if (!dangerousHtml) return;
+  const parser = new DOMParser();
+  const descriptionDocument = parser.parseFromString(
+    dangerousHtml,
+    "text/html",
+  );
+  for (const anchor of descriptionDocument.getElementsByTagName("a")) {
+    anchor.target = "_blank";
+    anchor.rel = "noopener noreferrer";
+  }
+  return DOMPurify.sanitize(descriptionDocument.body.innerHTML);
+}
