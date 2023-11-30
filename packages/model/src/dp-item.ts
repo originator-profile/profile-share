e2be05a5492000588b21a0451030db7fa1c473ab
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-11-30 13:32:08 +0900

    Advertisement型をDpItemに追加

--- packages/model/src/dp-item.ts
+++ packages/model/src/dp-item.ts
@@ -3,10 +3,11 @@ import DpVisibleText from "./dp-visible-text";
 import DpText from "./dp-text";
 import DpHtml from "./dp-html";
 import OgWebsite from "./og-website";
+import Advertisement from "./advertisement";
 
 const DpItem = {
   title: "Document Profile Item",
-  anyOf: [DpVisibleText, DpText, DpHtml, OgWebsite],
+  anyOf: [DpVisibleText, DpText, DpHtml, OgWebsite ,Advertisement],
 } as const;
 
 type DpItem = FromSchema<typeof DpItem>;
