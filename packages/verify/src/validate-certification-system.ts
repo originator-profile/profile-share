4db3dc1ea729544702b17f7eb5854dab3c8809bb
Kimiaki Kuno <knokmki612@gmail.com>
2023-12-01 15:46:00 +0900

    feat: 素朴な実装で検証する
    Ajv.compile を回避してより広範なランタイムで動作するようにします

--- packages/verify/src/validate-certification-system.ts
+++ packages/verify/src/validate-certification-system.ts
@@ -1,13 +1,52 @@
-import Ajv from "ajv";
+import compare from "just-compare";
 import { CertificationSystemValidationFailed } from "./errors";
 import { CertificationSystem } from "@originator-profile/model";
 
 /** 認証制度ペイロードの確認のためのバリデーター */
 export function CertificationSystemValidator() {
-  const ajv = new Ajv();
-  const validateCertificationSystemPayload = ajv.compile(CertificationSystem);
-  return { ajv, validateCertificationSystemPayload };
+  return function validate(
+    payload: unknown,
+  ): true | CertificationSystemValidationFailed {
+    if (typeof payload !== "object")
+      return new CertificationSystemValidationFailed("should be an object", {
+        payload,
+      });
+    if (payload === null)
+      return new CertificationSystemValidationFailed("should be an object", {
+        payload,
+      });
+    if (
+      !compare(
+        Object.keys(payload),
+        Object.keys(CertificationSystem.properties),
+      )
+    )
+      return new CertificationSystemValidationFailed(
+        "should be contain required properties or no contain the other properties",
+        { payload },
+      );
+    const entries = Object.entries(payload);
+    for (const entry of entries) {
+      const [key, value] = entry;
+      if (key === "type" && value !== CertificationSystem.properties.type.const)
+        return new CertificationSystemValidationFailed(
+          "should be contain value of 'certification-system' in 'type' property",
+          { payload },
+        );
+      if (key === "type") continue;
+      if (typeof value !== "string")
+        return new CertificationSystemValidationFailed(
+          `should be contain string value in '${key}' property`,
+          { payload },
+        );
+    }
+    return true;
+  };
 }
+
+export type CertificationSystemValidator = ReturnType<
+  typeof CertificationSystemValidator
+>;
 
 /**
  * 認証制度の検証
@@ -18,16 +57,9 @@ export function validateCertificationSystem(
   payload: unknown,
 ): CertificationSystem | CertificationSystemValidationFailed {
   const validator = CertificationSystemValidator();
-  const valid = validator.validateCertificationSystemPayload(payload);
-  if (!valid) {
-    const errors = validator.validateCertificationSystemPayload.errors;
-    return new CertificationSystemValidationFailed(
-      validator.ajv.errorsText(errors),
-      {
-        errors: errors ?? [],
-        payload,
-      },
-    );
+  const result = validator(payload);
+  if (result !== true) {
+    return result;
   }
-  return payload;
+  return payload as CertificationSystem;
 }
