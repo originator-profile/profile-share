0aa1d8f5d57fb7f4f5314439e6224ee82c79f2eb
Shuuichirou Murata <31531654+mrt0a@users.noreply.github.com>
2023-12-07 13:33:30 +0900

    拡張機能でwebsite Profile Pairを表示する (#1063)
    * feat(web-ext): website 検証情報の表示(初版)
    
    * bot: format [skip ci]
    
    * fix(web-ext): website Profile Pairが無効の場合は記事のOP/DPが無効と同様に扱う
    
    * fix: website Profileについては有効であればissuer,subjectで選択しない
    
    * fix: website ProfileからのOP/DP取得でissuer,subjectを使用していないことを明確化
    
    ---------
    
    Co-authored-by: bot <bot@originator-profile.org>

--- apps/web-ext/src/pages/Publ.tsx
+++ apps/web-ext/src/pages/Publ.tsx
@@ -1,39 +1,29 @@
+import { isValidElement } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import { isOp, isOpHolder, isDp, isOgWebsite } from "@originator-profile/core";
+import { Profile } from "@originator-profile/model";
 import useProfileSet from "../utils/use-profile-set";
 import { routes } from "../utils/routes";
-import Loading from "../components/Loading";
 import NotFound from "../components/NotFound";
 import Template from "../templates/Publ";
 import Unsupported from "../components/Unsupported";
 import useVerifyFailureFeedback from "../utils/use-verify-failure-feedback";
 
-function Publ() {
-  const [queryParams] = useSearchParams();
-  const { issuer, subject } = useParams<{ issuer: string; subject: string }>();
-  const { tabId, profiles, main = [], error } = useProfileSet();
-
-  const element = useVerifyFailureFeedback({
-    profiles,
-    tabId,
-    queryParams,
-  });
-
-  if (element) {
-    return element;
-  }
-
-  if (error) {
-    return <Unsupported error={error} />;
-  }
-  if (!profiles) {
-    return <Loading />;
-  }
-
-  const dp = profiles
-    .filter(isDp)
-    .find((dp) => dp.issuer === issuer && dp.subject === subject);
-  const op = profiles.filter(isOp).find((op) => op.subject === issuer);
+function extractFromProfiles(
+  profiles: Profile[],
+  queryParams: URLSearchParams,
+  issuer?: string,
+  subject?: string,
+  findFirstOpDp?: boolean,
+) {
+  const dp = findFirstOpDp
+    ? profiles.find(isDp)
+    : profiles
+        .filter(isDp)
+        .find((dp) => dp.issuer === issuer && dp.subject === subject);
+  const op = findFirstOpDp
+    ? profiles.find(isOp)
+    : profiles.filter(isOp).find((op) => op.subject === issuer);
   if (!(dp && op)) {
     return <NotFound variant="profile" />;
   }
@@ -45,7 +35,6 @@ function Publ() {
   if (!holder) {
     return <NotFound variant="holder" />;
   }
-
   const paths = {
     org: {
       pathname: routes.org.build({
@@ -56,15 +45,59 @@ function Publ() {
     },
   } as const;
 
+  return {
+    op,
+    dp,
+    website,
+    holder,
+    paths,
+  };
+}
+
+function Publ() {
+  const [queryParams] = useSearchParams();
+  const { issuer, subject } = useParams<{ issuer: string; subject: string }>();
+  const {
+    tabId,
+    profiles = [],
+    main = [],
+    error,
+    website: websiteProfiles = [],
+  } = useProfileSet();
+
+  const element = useVerifyFailureFeedback({
+    profiles: [...profiles, ...websiteProfiles],
+    tabId,
+    queryParams,
+  });
+
+  if (element) {
+    return element;
+  }
+  if (error) {
+    return <Unsupported error={error} />;
+  }
+
+  const article = extractFromProfiles(profiles, queryParams, issuer, subject);
+  const website = extractFromProfiles(
+    websiteProfiles,
+    queryParams,
+    undefined,
+    undefined,
+    true,
+  );
+  /* 記事について ReactElement(有効なプロファイルがない)なら記事のElementを表示することにする */
+  /* そもそもそのケースではPublが呼ばれないはずではある */
+  if (isValidElement(article)) {
+    return article;
+  }
+
   return (
     <Template
-      profiles={profiles}
-      main={main}
-      op={op}
-      dp={dp}
-      website={website}
-      holder={holder}
-      paths={paths}
+      /* ここに来る場合は article はかならずdpを持つはずだが型チェックがエラーにならないように */
+      article={"dp" in article ? { profiles, main, ...article } : undefined}
+      /* 技術情報の表示を実装する際にはwebsiteProfiles を追加する必要がある */
+      website={"dp" in website ? website : undefined}
     />
   );
 }
