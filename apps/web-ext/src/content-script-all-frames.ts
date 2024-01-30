8d64ae4e0619d36664defa1a5d20c69701685b79
Kimiaki Kuno <knokmki612@gmail.com>
2024-01-31 03:26:47 +0900

    fix: すべてのフレームでオーバーレイ表示される
    オーバーレイ表示関連のメッセージは最上位フレームでのみ処理する
    
    Co-authored-by: Yuki Soma <somay.work@gmail.com>

---
+++ apps/web-ext/src/content-script-all-frames.ts
@@ -0,0 +1,45 @@
+import { fetchProfileSet } from "@originator-profile/verify";
+import {
+  ContentScriptMessageRequest,
+  ContentScriptMessageResponse,
+  ContentWindowPostMessageEvent,
+} from "./types/message";
+
+async function handleMessageResponse(
+  message: ContentScriptMessageRequest,
+): Promise<ContentScriptMessageResponse | void> {
+  switch (message.type) {
+    case "fetch-profiles": {
+      const data = await fetchProfileSet(document);
+      return {
+        type: "fetch-profiles",
+        ok: !(data instanceof Error),
+        data:
+          data instanceof Error
+            ? JSON.stringify(data, Object.getOwnPropertyNames(data))
+            : JSON.stringify(data),
+        origin: document.location.origin,
+      };
+    }
+  }
+}
+
+chrome.runtime.onMessage.addListener(function (
+  message: ContentScriptMessageRequest,
+  _,
+  sendResponse: (response: ContentScriptMessageResponse | void) => void,
+): true /* NOTE: Chrome の場合、Promise には非対応 */ {
+  handleMessageResponse(message).then(
+    (response) => response && sendResponse(response),
+  );
+  return true;
+});
+
+function handlePostMessageResponse(event: ContentWindowPostMessageEvent) {
+  switch (event.data.type) {
+    default:
+      break;
+  }
+}
+
+window.addEventListener("message", handlePostMessageResponse);
