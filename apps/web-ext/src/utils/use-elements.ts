0b47997935d9a52e129ec55b563c11a7e2673830
Kimiaki Kuno <knokmki612@gmail.com>
2024-02-15 13:49:42 +0900

    fix: lint error

--- apps/web-ext/src/utils/use-elements.ts
+++ apps/web-ext/src/utils/use-elements.ts
@@ -14,21 +14,22 @@ function useElements(dp: Dp | Dp[]) {
   const iframeLocations = dps
     .filter((dp) => !dp.containTopLevelFrame)
     .map((dp) => `iframe[data-document-profile-subjects~="${dp.subject}"]`);
-  const selector = [...locations, ...iframeLocations].join(", ") || ":not(*)";
-  const [update, setUpdate] = useState<UpdateOverlayMessage | null>(null);
+  const [iframeReady, setIframeReady] = useState<UpdateOverlayMessage>();
   function handleMessage(event: IFramePostMessageEvent) {
     if (event.origin !== window.parent.location.origin) return;
     switch (event.data.type) {
       case "update-overlay":
-        setUpdate(event.data);
+        setIframeReady(event.data);
         break;
     }
   }
   useEvent("message", handleMessage);
-  const elements = useMemo<NodeListOf<HTMLElement>>(
-    () => window.parent.document.querySelectorAll(selector),
-    [location, update],
-  );
+  const elements = useMemo<NodeListOf<HTMLElement>>(() => {
+    const selector =
+      locations.concat(iframeReady ? iframeLocations : []).join(", ") ||
+      ":not(*)";
+    return window.parent.document.querySelectorAll(selector);
+  }, [locations, iframeLocations, iframeReady]);
   return { elements };
 }
 
