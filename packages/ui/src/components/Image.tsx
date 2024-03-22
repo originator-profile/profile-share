a3f772537604fce92d0220ef183f518262992978
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2024-03-22 17:20:32 +0900

    widthとheightを戻す、両方のパラメータ未指定で可変になるように修正

--- packages/ui/src/components/Image.tsx
+++ packages/ui/src/components/Image.tsx
@@ -1,3 +1,49 @@
+// import clsx from "clsx";
+
+// type Props = {
+//   className?: string;
+//   src?: string;
+//   placeholderSrc: string;
+//   alt: string;
+//   width?: number;
+//   height?: number;
+//   cover?: boolean;
+// };
+
+// function Image({ 
+//   className, 
+//   src, 
+//   placeholderSrc, 
+//   alt, 
+//   width, 
+//   height,
+//   cover = false,
+// }: Props) {
+
+//   return (
+//     <figure
+//       className={clsx(
+//         "flex justify-center items-center overflow-hidden",
+//         className,
+//       )}
+//       style={{ height, minWidth: width }}
+//     >
+//       <img
+//         className={clsx("w-full h-auto", 
+//         { 'object-cover': cover, 'object-contain': !cover }
+//         )}
+//         src={src ?? placeholderSrc}
+//         alt={alt}
+//         style={{ maxWidth: "240px", maxHeight: "44px" }}
+//         crossOrigin="anonymous"
+//       />
+//     </figure>
+//   );
+// }
+
+
+// export default Image;
+
 import clsx from "clsx";
 
 type Props = {
@@ -10,16 +56,15 @@ type Props = {
   cover?: boolean;
 };
 
-function Image({ 
-  className, 
-  src, 
-  placeholderSrc, 
-  alt, 
-  width, 
+function Image({
+  className,
+  src,
+  placeholderSrc,
+  alt,
+  width,
   height,
   cover = false,
 }: Props) {
-
   return (
     <figure
       className={clsx(
@@ -29,17 +74,20 @@ function Image({
       style={{ height, minWidth: width }}
     >
       <img
-        className={clsx("w-full h-auto", 
-        { 'object-cover': cover, 'object-contain': !cover }
-        )}
+        className={clsx("w-auto", {
+           'object-cover': cover, 'object-contain': !cover 
+        })}
+        style={
+          cover ? { width, height } : { maxWidth: width ?? '240px', maxHeight: height ?? '44px' }
+        }
         src={src ?? placeholderSrc}
         alt={alt}
-        style={{ maxWidth: "240px", maxHeight: "44px" }}
+        width={width}
+        height={height}
         crossOrigin="anonymous"
       />
     </figure>
   );
 }
-
 
 export default Image;
