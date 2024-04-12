ce700cd600b2e6aee9d0aff89eb7192a430bb620
Kimiaki Kuno <knokmki612@gmail.com>
2024-04-12 13:23:13 +0900

    refactor: ヘルプテキストの見た目振る舞いを再利用可能にしておく

---
+++ packages/registry-ui/src/components/FormHelpText.tsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+import { Icon } from "@iconify/react";
+import { Modal, useModal } from "./dialog";
+
+type Props = {
+  className?: string;
+  label: string;
+  helpText: string;
+};
+
+function FormHelpText({ className, label, helpText }: Props) {
+  const modal = useModal();
+  return (
+    <>
+      <button
+        className={clsx("leading-none align-middle", className)}
+        onClick={modal.open}
+      >
+        <Icon
+          className="text-lg text-gray-400"
+          icon="material-symbols:help"
+          onClick={modal.open}
+        />
+      </button>
+      <Modal title={label} description={helpText} dialogRef={modal.ref} />
+    </>
+  );
+}
+
+export default FormHelpText;
