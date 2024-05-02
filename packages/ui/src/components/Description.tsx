afcda9be2a6d6f115678d89b5e5536bf66be8e99
Kimiaki Kuno <knokmki612@gmail.com>
2024-05-02 12:09:56 +0900

    feat(Description): 見出しなしの表示を考慮する

--- packages/ui/src/components/Description.tsx
+++ packages/ui/src/components/Description.tsx
@@ -4,19 +4,24 @@ import useSanitizedHtml from "../utils/use-sanitized-html";
 type Props = {
   className?: string;
   description: string;
+  onlyBody?: boolean;
 };
 
-function Description({ className, description }: Props) {
+function Description({ className, description, onlyBody = false }: Props) {
   const html = useSanitizedHtml(description);
+  const body = (
+    <div
+      className="prose prose-xs text-xs break-words"
+      dangerouslySetInnerHTML={{
+        __html: html ?? "",
+      }}
+    />
+  );
+  if (onlyBody) return body;
   return (
     <section className={twMerge("py-1", className)}>
-      <h2 className="mb-1 text-gray-500 font-normal">説明</h2>
-      <div
-        className="prose prose-xs text-xs break-words"
-        dangerouslySetInnerHTML={{
-          __html: html ?? "",
-        }}
-      />
+      <h2 className="mb-1 text-gray-600 font-normal">説明</h2>
+      {body}
     </section>
   );
 }
