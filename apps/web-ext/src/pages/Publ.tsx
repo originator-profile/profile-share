56172e887b1a19a8ca5cba9b0e841e67bf460ddc
Kimiaki Kuno <knokmki612@gmail.com>
2024-05-09 08:33:30 +0900

    feat(Org): プロパティの整理
    ProfileSet.getWebsiteProfilePair() の型を変更した影響で
    template/Org のプロパティの型と不一致になったので実装を見直します

--- apps/web-ext/src/pages/Publ.tsx
+++ apps/web-ext/src/pages/Publ.tsx
@@ -1,88 +1,60 @@
 import { useParams, useSearchParams } from "react-router-dom";
-import { OgWebsite } from "@originator-profile/model";
 import useProfileSet from "../utils/use-profile-set";
 import { routes } from "../utils/routes";
 import NotFound from "../components/NotFound";
 import Template from "../templates/Publ";
-import {
-  DocumentProfile,
-  OriginatorProfile,
-  ProfileSet,
-} from "@originator-profile/ui";
+import { DocumentProfile, OriginatorProfile } from "@originator-profile/ui";
+import { DpItemContent } from "@originator-profile/core";
+import { OpHolder, OgWebsite } from "@originator-profile/model";
 import Loading from "../components/Loading";
 
-function extractFromOpDp(
-  op: OriginatorProfile | undefined,
-  dp: DocumentProfile | undefined,
-) {
-  if (!(dp && op)) {
-    return <NotFound variant="profile" />;
-  }
-  const website = dp.findOgWebsiteItem();
-  const advertisement = dp.findAdvertisementItem();
-  const content = website || advertisement;
-  if (!content) {
-    return <NotFound variant="website" />;
-  }
+function getTemplateProps<
+  T extends boolean | undefined,
+  R = T extends true ? OgWebsite : DpItemContent,
+>({
+  op,
+  dp,
+  mustWebsite,
+  queryParams,
+}: {
+  op?: OriginatorProfile;
+  dp?: DocumentProfile;
+  mustWebsite?: T;
+  queryParams: URLSearchParams;
+}):
+  | {
+      op: OriginatorProfile;
+      dp: DocumentProfile;
+      dpItemContent: R;
+      holder: OpHolder;
+      paths: {
+        org: {
+          pathname: string;
+          search: string;
+        };
+      };
+    }
+  | undefined {
+  if (!(op && dp)) return;
   const holder = op.findHolderItem();
-  if (!holder) {
-    return <NotFound variant="holder" />;
-  }
-
+  const dpItemContent = (
+    mustWebsite
+      ? dp.findOgWebsiteItem()
+      : dp.findOgWebsiteItem() ?? dp.findAdvertisementItem()
+  ) as R | undefined;
+  if (!(holder && dpItemContent)) return;
   return {
     op,
     dp,
-    content,
+    dpItemContent,
     holder,
-  };
-}
-
-function extractFromProfiles(
-  profiles: ProfileSet,
-  queryParams: URLSearchParams,
-  issuer?: string,
-  subject?: string,
-) {
-  if (!issuer || !subject) {
-    return <NotFound variant="profile" />;
-  }
-  const dp = profiles.getDp(subject, issuer);
-  const op = profiles.getOp(issuer);
-  if (!(dp && op)) {
-    return <NotFound variant="profile" />;
-  }
-  const article = extractFromOpDp(op, dp);
-  if (!("dp" in article)) {
-    return article;
-  }
-  return {
-    ...article,
     paths: {
       org: {
         pathname: routes.org.build(
-          routes.org.getParams({ contentType: dp.getContentType(), op }),
-        ),
-        search: queryParams.toString(),
-      },
-    },
-  };
-}
-
-function extractFromProfilePair(
-  profiles: ProfileSet,
-  queryParams: URLSearchParams,
-) {
-  const { op, dp } = profiles.getWebsiteProfilePair();
-  const website = extractFromOpDp(op, dp);
-  if (!("dp" in website)) {
-    return website;
-  }
-  return {
-    ...website,
-    paths: {
-      org: {
-        pathname: routes.org.build(
-          routes.org.getParams({ contentType: "サイト", op }),
+          routes.org.getParams({
+            contentType: mustWebsite ? "サイト" : dp.getContentType(),
+            op,
+          }),
         ),
         search: queryParams.toString(),
       },
@@ -92,9 +64,13 @@ function extractFromProfilePair(
 
 function Publ() {
   const [queryParams] = useSearchParams();
-  const { issuer, subject, tabId } = useParams<{
-    issuer: string;
-    subject: string;
+  const {
+    issuer = "",
+    subject = "",
+    tabId,
+  } = useParams<{
+    issuer?: string;
+    subject?: string;
     tabId: string;
   }>();
 
@@ -103,9 +79,19 @@ function Publ() {
   if (profileSet.isLoading) {
     return <Loading />;
   }
-
-  const article = extractFromProfiles(profileSet, queryParams, issuer, subject);
-  const website = extractFromProfilePair(profileSet, queryParams);
+  const article = getTemplateProps({
+    op: profileSet.getOp(issuer),
+    dp: profileSet.getDp(subject, issuer),
+    queryParams,
+  });
+  const website = getTemplateProps({
+    ...profileSet.getWebsiteProfilePair(),
+    mustWebsite: true,
+    queryParams,
+  });
+  if (!(article || website)) {
+    return <NotFound variant="profile" />;
+  }
 
   const handleClickDp = (dp: DocumentProfile) => async () => {
     await chrome.tabs.sendMessage(Number(tabId), {
@@ -117,22 +103,9 @@ function Publ() {
 
   return (
     <Template
-      /* ここに来る場合は article はかならずdpを持つはずだが型チェックがエラーにならないように */
-      article={
-        "dp" in article
-          ? {
-              profiles: profileSet,
-              dpItemContent: article.content,
-              ...article,
-            }
-          : undefined
-      }
-      /* 技術情報の表示を実装する際にはwebsiteProfiles を追加する必要がある */
-      website={
-        "dp" in website
-          ? { website: website.content as OgWebsite, ...website }
-          : undefined
-      }
+      profiles={profileSet}
+      article={article}
+      website={website}
       handleClickDp={handleClickDp}
     />
   );
