04b4e6df3870667ddbdfa6d365a904cbe8bcdce3
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2024-07-04 11:34:02 +0900

    APIドキュメントにsummaryとexamplesを追加

--- apps/registry/src/routes/get-jwt-vc-issuer.ts
+++ apps/registry/src/routes/get-jwt-vc-issuer.ts
@@ -5,6 +5,7 @@ import { Jwks } from "@originator-profile/model";
 const schema: FastifySchema = {
   operationId: "getJwtVcIssuer",
   tags: ["registry"],
+  summary: "OPレジストリ JWT VC Issuer Metadata の取得",
   response: {
     200: {
       type: "object",
@@ -13,7 +14,23 @@ const schema: FastifySchema = {
         jwks: Jwks,
       },
       required: ["issuer", "jwks"],
-      additionalProperties: false
+      additionalProperties: false,
+      examples: [
+        {
+          issuer: "https://example.com",
+          jwks: {
+            keys: [
+              {
+                kty: "EC",
+                kid: "jJYs5_ILgUc8180L-pBPxBpgA3QC7eZu9wKOkh9mYPU",
+                x: "ypAlUjo5O5soUNHk3mlRyfw6ujxqjfD_HMQt7XH-rSg",
+                y: "1cmv9lmZvL0XAERNxvrT2kZkC4Uwu5i1Or1O-4ixJuE",
+                crv: "P-256"
+              }
+            ]
+          }
+        }
+      ]
     }
   }
 };
