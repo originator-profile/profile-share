33bb2cd4fa3fbbe6b84e3ee8b5fa9fc12b692c67
Kimiaki Kuno <knokmki612@gmail.com>
2024-07-11 19:27:34 +0900

    fix: 自動テスト用JWTの有効期限切れ (#1503)
    自動テスト用JWTをテスト実行時生成することで対処します

--- apps/registry/e2e/profile-set.test.ts
+++ apps/registry/e2e/profile-set.test.ts
@@ -1,20 +1,36 @@
 import { afterAll, beforeAll, describe, expect, test } from "vitest";
 import jsonld from "jsonld";
+import { addYears } from "date-fns";
+import { Dp, DpText } from "@originator-profile/model";
+import { signDp, signBody } from "@originator-profile/sign";
 import {
   expandProfileSet,
   ProfilesVerifier,
   RemoteKeys,
 } from "@originator-profile/verify";
+import privateKey from "../account-key.example.priv.json";
 
 const accountId = "cd8f5f9f-e3e8-569f-87ef-f03c6cfc29bc";
 const dummyPassword = "bdf70f3d38c1311fa06a211a2205623a";
 
-const dummyId = "d4f2c5b6-129d-44fe-a441-837b808ef643";
-const dummyJwt =
-  "eyJhbGciOiJFUzI1NiIsImtpZCI6ImpKWXM1X0lMZ1VjODE4MEwtcEJQeEJwZ0EzUUM3ZVp1OXdLT2toOW1ZUFUiLCJ0eXAiOiJKV1QifQ.eyJodHRwczovL29yaWdpbmF0b3ItcHJvZmlsZS5vcmcvZHAiOnsiaXRlbSI6W3sidHlwZSI6IndlYnNpdGUiLCJ1cmwiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJ0aXRsZSI6Ik9QIOeiuuiqjeOBj-OCkyIsImltYWdlIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2Fzc2V0cy9sb2dvLTJiMDRlNjM1LnN2ZyIsImNhdGVnb3J5IjpbXX0seyJ0eXBlIjoidmlzaWJsZVRleHQiLCJ1cmwiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJsb2NhdGlvbiI6ImgxIiwicHJvb2YiOnsiandzIjoiZXlKaGJHY2lPaUpGVXpJMU5pSXNJbXRwWkNJNkltcEtXWE0xWDBsTVoxVmpPREU0TUV3dGNFSlFlRUp3WjBFelVVTTNaVnAxT1hkTFQydG9PVzFaVUZVaUxDSmlOalFpT21aaGJITmxMQ0pqY21sMElqcGJJbUkyTkNKZGZRLi4wR1RTSlNBSHVZU2FEV0RTcmczRDBXUDF5Y0xHdUp3WW9WNUpyR0NULV9QUXZtdzJlcGNtc0h0ZWpDelkzUk95LTJYY0JQNFZ5MW55ZGhqVXctMXpTdyJ9fV19LCJpc3MiOiJsb2NhbGhvc3QiLCJzdWIiOiJENEYyQzVCNi0xMjlELTQ0RkUtQTQ0MS04MzdCODA4RUY2NDMiLCJpYXQiOjE2ODkwNTkzODcsImV4cCI6MTcyMDY4MTc4N30.0-hvWu8lz8WuLJzIRFUdYJfn6YIhi7MrYR2F9XYMKhjHRQcGkYVjFU-GAmZYR0BP-EPnUuoKszzD5DhfhOQOxg";
+const dp: Dp = {
+  type: "dp",
+  issuedAt: new Date().toISOString(),
+  expiredAt: addYears(new Date(), 1).toISOString(),
+  issuer: "localhost",
+  subject: "d4f2c5b6-129d-44fe-a441-837b808ef643",
+  item: [],
+};
 
 describe("複数のSigned Document Profilesが存在する場合", () => {
   beforeAll(async () => {
+    const text: DpText = {
+      type: "text",
+      location: ":not(*)",
+      proof: { jws: await signBody("dummy", privateKey) },
+      url: "https://dummy.example.com/",
+    };
+    const jwt = await signDp({ ...dp, item: [text] }, privateKey);
     const response = await fetch(
       `http://localhost:8080/admin/publisher/${accountId}/dp/`,
       {
@@ -26,7 +42,7 @@ describe("複数のSigned Document Profilesが存在する場合", () => {
           "content-type": "application/json",
         },
         body: JSON.stringify({
-          jwt: dummyJwt,
+          jwt,
         }),
       },
     );
@@ -51,7 +67,7 @@ describe("複数のSigned Document Profilesが存在する場合", () => {
         },
         body: JSON.stringify({
           input: {
-            id: dummyId,
+            id: dp.subject,
           },
         }),
       },
