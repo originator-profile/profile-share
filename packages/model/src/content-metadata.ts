2011f0b2abcc357f88160b7d859dd484016162b6
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2024-08-13 10:44:57 +0900

    contentをrefを用いずに直接定義、参照するように変更

--- packages/model/src/content-metadata.ts
+++ packages/model/src/content-metadata.ts
@@ -3,6 +3,72 @@ import Category from "./category";
 import AllowedUrls from "./allowed-urls";
 import AllowedOrigins from "./allowed-origins";
 
+const content = {
+  type: "object",
+  additionalProperties: true,
+  properties: {
+    title: {
+      type: "string",
+      description: "コンテンツのタイトル。",
+    },
+    description: {
+      type: "string",
+      description: "コンテンツの説明（プレーンテキスト）。",
+    },
+    source: {
+      title: "一次ソース",
+      type: "string",
+      format: "uri",
+      description:
+        "コンテンツの流通における1次ソース URL がある場合は記載を推奨 (RECOMMENDED)。",
+    },
+    image: {
+      type: "string",
+      format: "uri",
+      description:
+        "コンテンツの画像の URL。コンテンツに強い関連をもつ画像があるならば、その画像を指定するべきです (RECOMMENDED)。",
+    },
+    "image#integrity": {
+      type: "string",
+      description:
+        "コンテンツの画像のハッシュ値を指定します。ハッシュ値の形式は Subresource Integrity セクション 3.1 の Integrity metadata でなければなりません (MUST)。",
+    },
+    date_published: {
+      title: "公開日",
+      type: "string",
+    },
+    date_modified: {
+      title: "最終更新日",
+      type: "string",
+    },
+    editors: {
+      title: "編集者名",
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+    authors: {
+      title: "著者名",
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+    categories: {
+      type: "array",
+      items: Category,
+      description:
+        "IAB カテゴリータクソノミーによる分類の JSON 配列。空配列でもよい (MAY)。",
+    },
+    locale: {
+      type: "string",
+      description: "Web コンテンツのロケール。",
+    },
+  },
+  required: ["title", "description", "locale"],
+} as const;
+
 const contentMetadata = {
   $schema: "https://json-schema.org/draft/2020-12/schema",
   title: "Content Metadata",
@@ -18,74 +84,7 @@ const contentMetadata = {
     allowed_origins: AllowedOrigins,
   },
   required: ["vct", "allowed_urls"],
-  $defs: {
-    content: {
-      type: "object",
-      additionalProperties: true,
-      properties: {
-        title: {
-          type: "string",
-          description: "コンテンツのタイトル。",
-        },
-        description: {
-          type: "string",
-          description: "コンテンツの説明（プレーンテキスト）。",
-        },
-        source: {
-          title: "一次ソース",
-          type: "string",
-          format: "uri",
-          description:
-            "コンテンツの流通における1次ソース URL がある場合は記載を推奨 (RECOMMENDED)。",
-        },
-        image: {
-          type: "string",
-          format: "uri",
-          description:
-            "コンテンツの画像の URL。コンテンツに強い関連をもつ画像があるならば、その画像を指定するべきです (RECOMMENDED)。",
-        },
-        "image#integrity": {
-          type: "string",
-          description:
-            "コンテンツの画像のハッシュ値を指定します。ハッシュ値の形式は Subresource Integrity セクション 3.1 の Integrity metadata でなければなりません (MUST)。",
-        },
-        date_published: {
-          title: "公開日",
-          type: "string",
-        },
-        date_modified: {
-          title: "最終更新日",
-          type: "string",
-        },
-        editors: {
-          title: "編集者名",
-          type: "array",
-          items: {
-            type: "string",
-          },
-        },
-        authors: {
-          title: "著者名",
-          type: "array",
-          items: {
-            type: "string",
-          },
-        },
-        categories: {
-          type: "array",
-          items: Category,
-          description:
-            "IAB カテゴリータクソノミーによる分類の JSON 配列。空配列でもよい (MAY)。",
-        },
-        locale: {
-          type: "string",
-          description: "Web コンテンツのロケール。",
-        },
-      },
-      required: ["title", "description", "locale"],
-    },
-  },
-  allOf: [{ $ref: "#/$defs/content" }],
+  allOf: [content],
 } as const;
 
 type ContentMetadata = FromSchema<typeof contentMetadata>;
