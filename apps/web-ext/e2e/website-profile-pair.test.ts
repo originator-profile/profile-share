85bf7b22b7d17236622bfbc6e0462e6c6861e80e
somay <somay@users.noreply.github.com>
2024-10-02 06:12:42 +0900

    fix(web-ext): 拡張機能全体で credentials "include" mode 使用 (#1626)
    * fix(web-ext): ポップアップウィンドウでも credentials "include" mode 使用
    
    * fetch の上書きを main.tsx に移動
    
    * web-ext: fetch で認証情報を付与する
    
    * web-ext: webRequest API 依存を排除
    
    * feat(web-ext): load イベントを待たずに content script 挿入
    
    * web-ext: 認証情報付与は fetch に集約
    
    * web-ext: 一部の iframe で失敗しても取得成功
    
    * refactor(web-ext): 関数分割
    
    * refactor(web-ext): 関数を分割
    
    * test(web-ext): サイトプロファイルのみのケース
    
    * webRequest API 復活
    
    * グローバル変数を定義
    
    * tsdoc 追加
    
    * 型名変更

--- apps/web-ext/e2e/website-profile-pair.test.ts
+++ apps/web-ext/e2e/website-profile-pair.test.ts
@@ -4,19 +4,16 @@ import { expect, popup, test } from "./fixtures";
 test.describe.configure({ mode: "serial" });
 let ext: Page | undefined;
 
-test.beforeEach(async ({ context, page }) => {
-  // Profile Registry にアクセス (apps/registry)
-  await page.goto("http://localhost:8080/examples/many-dps.html");
-
-  ext = await popup(context);
-});
-
 test.afterEach(async ({ page }, testInfo) => {
   await page.screenshot({ path: `screenshots/${testInfo.title}-本体.png` });
   await ext?.screenshot({ path: `screenshots/${testInfo.title}-拡張機能.png` });
 });
 
-test("サイトプロファイルにおける表示の確認", async ({ page }) => {
+test("サイトプロファイルにおける表示の確認", async ({ context, page }) => {
+  // Profile Registry にアクセス (apps/registry)
+  await page.goto("http://localhost:8080/examples/many-dps.html");
+
+  ext = await popup(context);
   expect(await ext?.title()).toMatch(/コンテンツ情報/);
 
   // サイトプロファイルの発行者を持つ要素が存在するか確認
@@ -40,4 +37,27 @@ test("サイトプロファイルにおける表示の確認", async ({ page }) 
   expect(
     await ext?.locator('h1:has-text("Website Profile Pair title")').count(),
   ).toEqual(1);
+});
+
+test("サイトプロファイルのみ表示の確認", async ({ context, page }) => {
+  // 存在しないページにアクセス
+  await page.goto("http://localhost:8080/examples/404.html");
+
+  ext = await popup(context);
+  expect(await ext?.title()).toMatch(/コンテンツ情報/);
+
+  // サイトプロファイルの発行者を持つ要素が存在するか確認
+  expect(await ext?.getByTestId("pp-json-holder").innerText()).toMatch(
+    /Originator Profile 技術研究組合/,
+  );
+
+  expect(
+    await ext
+      ?.locator(':text("このサイトの運営者には信頼性情報があります")')
+      .count(),
+  ).toEqual(1);
+
+  expect(
+    await ext?.locator(':text("の発行者には信頼性情報があります")').count(),
+  ).toEqual(0);
 });
--- apps/web-ext/src/content-script-all-frames.ts
+++ apps/web-ext/src/content-script-all-frames.ts
@@ -1,35 +1,9 @@
 import { extractBody, fetchProfileSet } from "@originator-profile/verify";
+import "./utils/cors-basic-auth";
 import {
   ContentScriptAllFramesMessageRequest,
   ContentScriptAllFramesMessageResponse,
 } from "./types/message";
-
-if (import.meta.env.BASIC_AUTH) {
-  const __fetch = globalThis.fetch;
-
-  globalThis.fetch = async function fetch(
-    input: RequestInfo | URL,
-    init?: RequestInit,
-  ): Promise<Response> {
-    const url = new URL(input instanceof Request ? input.url : input);
-    const auth = import.meta.env.BASIC_AUTH_CREDENTIALS.find(
-      ({ domain }) => domain === url.hostname,
-    );
-
-    return await __fetch(
-      input,
-      auth
-        ? {
-            ...init,
-            headers: {
-              authorization: `Basic ${btoa(`${auth.username}:${auth.password}`)}`,
-              ...init?.headers,
-            },
-          }
-        : init,
-    );
-  };
-}
 
 async function handleMessageResponse(
   message: ContentScriptAllFramesMessageRequest,
