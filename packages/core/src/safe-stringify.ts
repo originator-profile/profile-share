d7d202bf4d1ce46562af901d64554db34f573a54
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2024-10-23 12:46:41 +0900

    docs: stringifyWithErrorに関数ヘッダーを追加

--- packages/core/src/safe-stringify.ts
+++ packages/core/src/safe-stringify.ts
@@ -1,3 +1,8 @@
+/**
+ * Error クラスインスタンスを含むデータの文字列化
+ * @param data Error クラスインスタンスかもしれないデータ
+ * @returns JSON 文字列
+ **/
 export function stringifyWithError(data: unknown): string {
   return data instanceof Error
     ? JSON.stringify(data, Object.getOwnPropertyNames(data))
