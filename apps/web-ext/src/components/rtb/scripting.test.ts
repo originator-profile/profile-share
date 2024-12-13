fbdc22da4f793119178122cff82a486841f18c53
Kimiaki Kuno <knokmki612@gmail.com>
2024-12-13 11:39:47 +0900

    fix(rtb): デシリアライズした getJsonLdNodeObjects が呼び出せていない (#1852)
    `new Function(getJsonLdNodeObject.toString())` は
    `() => { function getJsonLdNodeObject(document) { } }` な関数
    を作っているので、関数定義されるが呼び出されない。
    Function コンストラクターに引数と呼び出しのコードの追加が必要だった

--- apps/web-ext/src/components/rtb/scripting.test.ts
+++ apps/web-ext/src/components/rtb/scripting.test.ts
@@ -3,7 +3,10 @@ import { expect, test } from "vitest";
 import { Window } from "happy-dom";
 
 test("getJsonLdNodeObjects()はchrome.scripting ScriptInjectionで使用しているのでシリアル化して呼び出せなければならない", () => {
-  const func = new Function(getJsonLdNodeObjects.toString());
+  const func = new Function(
+    "document",
+    `${getJsonLdNodeObjects.toString()}\ngetJsonLdNodeObjects(document);`,
+  );
   const window = new Window();
   expect(() => func(window.document)).not.toThrowError();
 });
