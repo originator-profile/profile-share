543e3186a60db5914a156459cbd17a1279ed8fb2
Shuuichirou Murata <31531654+mrt0a@users.noreply.github.com>
2024-12-25 17:24:00 +0900

    CASの検証エラーを拡張機能に表示する (#1880)
    * feat: CASの検証エラーを検出してprohibit
    
    * test: おかしなCASでprohibitページが表示される
    
    * test: タイトル修正, opsではなくcasを編集
    
    * bot: format [skip ci]
    
    ---------
    
    Co-authored-by: bot <bot@originator-profile.org>

---
+++ apps/web-ext/e2e/credentials-prohibition.test.ts
@@ -0,0 +1,30 @@
+import { expect, popup, test as base } from "./fixtures";
+import { ContentAttestationSet } from "@originator-profile/model";
+
+const test = base.extend({
+  page: async ({ page }, use) => {
+    /* Verify失敗するContent Attestation Set */
+    const cas: ContentAttestationSet = [
+      {
+        attestation: "",
+        main: true,
+      },
+    ];
+    await page.route("http://localhost:8080/examples/cas.json", async (route) =>
+      route.fulfill({
+        body: JSON.stringify(cas),
+        contentType: "application/json",
+      }),
+    );
+    await use(page);
+  },
+});
+
+test("Content Attestation Set の検証に失敗した場合", async ({
+  context,
+  page,
+}) => {
+  await page.goto("http://localhost:8080/examples/cas-2.html");
+  const ext = await popup(context);
+  await expect(ext?.getByTestId("p-elm-prohibition-message")).toBeVisible();
+});
