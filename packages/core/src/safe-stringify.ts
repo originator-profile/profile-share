ae27e88eabe88d956f53d36cfa0ff39ea703c243
kou_yeung <kou_yeung@hotmail.com>
2025-03-03 18:09:22 +0900

    Errorオブジェクト含むインスタンスのシリアライズ対応 (#1913)
    * Errorクラスを含むオブジェクトをシリアライズ実装
    
    * Errorクラスを含むオブジェクトをシリアライズ実装TEST
    
    * bot: format [skip ci]
    
    * use seialize-error to parse Error Object
    
    * bot: format [skip ci]
    
    * use type unknown
    
    * fix lint error : any
    
    * stringifyWithErrorを使ってシリアライズして使うためFetchCredentialsMessageResultやtoFetchCredentialsMessageResultを削除します
    
    * bot: format [skip ci]
    
    * mainブランチを取り込んで競合してた箇所を修正します
    
    * bot: format [skip ci]
    
    * エラーインスタンスのシリアライズ周り調整
    ・もう少し型を明確するようにもとの実装に少し近づくように修正しました
    
    * bot: format [skip ci]
    
    ---------
    
    Co-authored-by: bot <bot@originator-profile.org>

--- packages/core/src/safe-stringify.ts
+++ packages/core/src/safe-stringify.ts
@@ -1,3 +1,13 @@
+import { serializeError, deserializeError, isErrorLike } from "serialize-error";
+
+function replaceErrors(key: string, value: unknown): unknown {
+  if (value instanceof Error) {
+    return serializeError(value);
+  } else {
+    return value;
+  }
+}
+
 /**
  * Error クラスインスタンスを含むデータの文字列化
  * @param data Error クラスインスタンスかもしれないデータ
@@ -5,7 +15,31 @@
  * @returns JSON 文字列
  **/
 export function stringifyWithError(data: unknown, indent?: number): string {
-  return data instanceof Error
-    ? JSON.stringify(data, Object.getOwnPropertyNames(data), indent)
-    : JSON.stringify(data, null, indent);
+  return JSON.stringify(data, replaceErrors, indent);
+}
+
+function reviverErrors(key: string, value: unknown): unknown {
+  if (isErrorLike(value)) {
+    return deserializeError(value);
+  } else {
+    return value;
+  }
+}
+
+/**
+ * Error クラスインスタンスを含む文字列のデータ化
+ * @param text JSON 文字列
+ * @returns Error クラスインスタンスかもしれないデータ
+ **/
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export function parseWithError(text: string): any {
+  return JSON.parse(text, reviverErrors);
+}
+
+export function serializeIfError<T>(o: T): T | string {
+  return isErrorLike(o) ? (serializeError(o) as string) : o;
+}
+
+export function deserializeIfError<T>(o: T | string): T | Error {
+  return isErrorLike(o) ? deserializeError(o) : (o as T);
 }
