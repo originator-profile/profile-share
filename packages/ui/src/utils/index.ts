756f1460a3f0a3e886517d93492ba5c5d25d7b02
Shuuichirou Murata <31531654+mrt0a@users.noreply.github.com>
2025-03-24 14:01:37 +0900

    旧op/dpを操作するcore関数を削除 (#2025)
    * refactor: op/dpに関するcore関数を削除
    
    * fix: 一時的にtestファイルがなくなったので、その状態でエラーにならないように

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,9 +1,5 @@
 export * from "./role";
-export { default as toProfilePayload } from "./to-profile-payload";
 export { default as useModal } from "./use-modal";
 export { default as useSanitizedHtml } from "./use-sanitized-html";
 export { default as useCertificationSystem } from "./use-certification-system";
-export { default as getContentType } from "./get-content-type";
 export { _, getMessage } from "./get-message";
-export * from "./profile";
-export * from "./profile-set";
