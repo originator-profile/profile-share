d3ecad9d3849be1db5d84ed9511027d870a73ebf
YuukiTsuchida <dev.YuukiTsuchida@gmail.com>
2025-05-02 19:37:25 +0900

    test: site-profile-and-cas-missing.test.tsをfixtureを利用した実装に変更 (#2079)
    * test: site-profile-and-cas-missing.test.tsをfixtureを利用した実装に変更
    
    * Update apps/web-ext/e2e/credentials-fixtures.ts
    
    Co-authored-by: Kimiaki Kuno <knokmki612@gmail.com>
    
    ---------
    
    Co-authored-by: Kimiaki Kuno <knokmki612@gmail.com>

--- apps/web-ext/e2e/site-profile-and-cas-missing.test.ts
+++ apps/web-ext/e2e/site-profile-and-cas-missing.test.ts
@@ -1,24 +1,43 @@
-// TODO:
-// 名前等に違和感があるので
-// https://github.com/originator-profile/profile/issues/1925
-// このタスク対応でテストの見直しをしたほうが良さそう
 import { expect, popup, test as base } from "./fixtures";
+import { test as siteProfileTest } from "./site-profile-fixtures";
+import { test as staticHtmlTest } from "./static-html-fixtures";
+import { test as credentialsTest } from "./credentials-fixtures";
+import path from "node:path";
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { mergeTests } from "@playwright/test";
 
-const test = base.extend({
-  page: async ({ page }, use) => {
-    await page.route(
-      "http://localhost:8080/.well-known/sp.json",
-      async (route) =>
-        route.fulfill({
-          status: 404,
-        }),
-    );
-    await use(page);
-  },
-});
+const test = mergeTests(
+  base,
+  siteProfileTest,
+  staticHtmlTest,
+  credentialsTest,
+).extend({});
 
-test("Site Profile と CAS が取得できない場合", async ({ context, page }) => {
-  await page.goto("http://localhost:8080/examples/cas-missing.html");
+const cwd = path.dirname(fileURLToPath(new URL(import.meta.url)));
+const privKeyPath = path.join(
+  cwd,
+  "../../registry/account-key.example.priv.json",
+);
+const privKeyBuffer = await fs.readFile(privKeyPath);
+const privateKey = JSON.parse(privKeyBuffer.toString());
+
+const pubKeyPath = path.join(
+  cwd,
+  "../../registry/account-key.example.pub.json",
+);
+const pubKeyBuffer = await fs.readFile(pubKeyPath);
+const publicKey = JSON.parse(pubKeyBuffer.toString());
+test("Site Profile と CAS が取得できない場合Unsuportedが表示されるか", async ({
+  context,
+  page,
+  missingSiteProfile: _missingSiteProfile,
+  missingCas: _missingCas,
+  validOps,
+  credentialsPage,
+}) => {
+  await validOps({ publicKey, privateKey });
+  await page.goto(credentialsPage.endpoint);
   const ext = await popup(context);
   await expect(ext?.getByTestId("p-elm-unsupported-message")).toBeVisible();
   await expect(
