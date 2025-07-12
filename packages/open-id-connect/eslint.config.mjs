e376a9aae3db46185b9de4c3f03c3033f145c11d
Youhei Nozaki <youhey.hey0505@gmail.com>
2025-07-13 01:45:02 +0900

    feat(open-id-connect): add OpenID Connect package and initial implementation

---
+++ packages/open-id-connect/eslint.config.mjs
@@ -0,0 +1,3 @@
+import config from '@originator-profile/eslint-config/eslint.config.mjs'
+
+export default config
---
+++ packages/open-id-connect/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/server.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  target: 'node20',
+  outDir: 'dist',
+  splitting: false,
+  minify: false,
+});
