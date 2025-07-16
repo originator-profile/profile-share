b6a1a5fd20306f3bfa177fe6497ebbe4113be4c8
Sa-R390 <saryo@acutus.jp>
2025-07-16 11:03:31 +0900

    add:英語版デモページの追加

--- packages/demo-page/vite.config.js
+++ packages/demo-page/vite.config.js
@@ -1,8 +1,16 @@
+// vite.config.ts
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+  base: "./",
   build: {
     rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        "ja/index": path.resolve(__dirname, "ja/index.html"),
+        "en/index": path.resolve(__dirname, "en/index.html"),
+      },
       output: {
         // assets/images の中に元のファイル名で出力
         assetFileNames: "assets/images/[name].[ext]",
