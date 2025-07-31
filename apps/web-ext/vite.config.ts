1e5df0f207c7033abc74d63aaadd4af86147fe21
Sa-R390 <saryo@acutus.jp>
2025-07-31 16:03:54 +0900

    update:静的ファイルをpublicディレクトリ以下に移動

--- apps/web-ext/vite.config.ts
+++ apps/web-ext/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 export default defineConfig({
   // publicDir を playwright フォルダに
   root: "playwright",
-  publicDir: path.resolve(__dirname, "playwright"),
+  publicDir: path.resolve(__dirname, "playwright/public"),
   server: {
     port: 8080,
   },
