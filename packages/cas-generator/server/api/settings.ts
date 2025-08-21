8c94c16061d289a7d6cf801c668f62f2027c5bf4
Youhei Nozaki <youhey.hey0505@gmail.com>
2025-08-21 22:46:26 +0900

    refactor: remove unused environment variables from settings
    - Removed unnecessary environment variables related to OAuth configuration from the settings file to streamline the code and improve clarity.

--- packages/cas-generator/server/api/settings.ts
+++ packages/cas-generator/server/api/settings.ts
@@ -4,10 +4,5 @@ export default defineEventHandler(async () => {
     VC_OUTPUT_PATH: process.env.VC_OUTPUT_PATH,
     CAS_OUTPUT_PATH: process.env.CAS_OUTPUT_PATH,
     PRIVATE_KEY_PATH: process.env.PRIVATE_KEY_PATH,
-    ISSUER: process.env.ISSUER,
-    AUTHORIZATION_ENDPOINT: process.env.AUTHORIZATION_ENDPOINT,
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-    REDIRECT_URI: process.env.REDIRECT_URI,
   };
 });
