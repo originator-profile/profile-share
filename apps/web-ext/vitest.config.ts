7616fa0e9901683e4e134c1427cf446ff4af18ac
Kohei Watanabe <nebel@fogtype.com>
2025-09-18 22:05:17 +0900

    fix: publish 用 package.json の調整しビルド可能に修正

--- apps/web-ext/vitest.config.ts
+++ apps/web-ext/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   ssr: {
     resolve: {
-      conditions: ["browser", "typescript"],
+      conditions: ["browser"],
     },
   },
   test: {
--- packages/sign/vitest.config.ts
+++ packages/sign/vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    conditions: ["typescript"],
-  },
   test: {
     environment: "happy-dom",
   },
