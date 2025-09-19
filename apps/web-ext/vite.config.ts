197ce82176ffb2616feef4bafb0dabdd366c59dc
Kohei Watanabe <nebel@fogtype.com>
2025-09-19 11:04:13 +0900

    feat: OPSとCASのデモページにアクセスして検証できるように配置 (#142)
    Co-authored-by: Sa-R390 <saryo@acutus.jp>

--- apps/web-ext/vite.config.ts
+++ apps/web-ext/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
-  // publicDir を playwright フォルダに
   root: "playwright",
-  publicDir: path.resolve(__dirname, "playwright/public"),
   server: {
     port: 8080,
   },
